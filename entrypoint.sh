20a820b2cb20bc0d48d81130f72967d3a281d874
qingyi <qingyiwang@xiaohongshu.com>
2019-02-28 20:33:39 +0800

    test

---
+++ entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+
+exec 2>&1
+
+cd /data/litatom
+
+python <<EOF
+import yaml
+import json
+import sys
+
+# Load env var from YAML
+with open("ansible/online_conf") as f:
+    envf = yaml.load(f)
+
+for key in envf['litatom_envvars']:
+    with open('.env/' + key, 'w') as f:
+        f.write(str(envf['litatom_envvars'][key]))
+
+for key in envf['litatom_envvars_json']:
+    with open('.env/' + key, 'w') as f:
+        f.write(json.dumps(envf['litatom_envvars_json'][key]))
+EOF
+
+#exec chpst -e .env bin/serve.py
---
+++ litatom/db.py
@@ -0,0 +1,66 @@
+import logging
+
+from hendrix.conf import setting
+from .metrics import metrics
+from .util import BaseDBManager
+
+import flask
+import mongoengine
+from pymongo import monitoring
+from pymongo.errors import AutoReconnect
+
+logger = logging.getLogger(__name__)
+
+
+def retry(func, times=3, *args, **kwargs):
+    for i in range(times):
+        try:
+            return func(*args, **kwargs)
+        except AutoReconnect as e:
+            if i >= times - 1:
+                raise
+            logger.warning('retry db operation on exc: %r', e)
+
+
+class DBManager(BaseDBManager):
+    @property
+    def settings(self):
+        return setting.DB_SETTINGS
+
+    def _initdb(self, name):
+        opts = self.settings[name].copy()
+        self[name] = mongoengine.connect(**opts)
+
+
+class CommandLogger(monitoring.CommandListener):
+    start = {}
+
+    def _send_metric(self, evt, ok):
+        ns = ''
+        if ok and 'cursor' in evt.reply:
+            ns = evt.reply['cursor']['ns']
+        metrics['mongo.ops'].\
+            tags(ns=ns, op=evt.command_name, ok=ok,
+                 hostname=metrics.HOSTNAME).\
+            values(rid=flask.request.id if flask.request else '',
+                   req_id=evt.request_id,
+                   op_id=evt.operation_id).\
+            commit(evt.duration_micros / 1000)
+
+    def started(self, evt):
+        pass
+
+    def succeeded(self, evt):
+        self._send_metric(evt, True)
+
+    def failed(self, evt):
+        self._send_metric(evt, False)
+        logger.error("%r failed on %s: %r %r %dus",
+                     evt.command_name,
+                     evt.connection_id,
+                     evt.failure,
+                     evt.request_id,
+                     evt.duration_micros,
+                     )
+
+#monitoring.register(CommandLogger())
