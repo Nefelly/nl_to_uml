5431b0c88e86fac610fd99a4ad519fc1623ef162
qingyi <qingyiwang@xiaohongshu.com>
2019-03-04 21:32:20 +0800

    test

---
+++ litatom/api/v1/endpoint/image.py
@@ -0,0 +1,54 @@
+# coding: utf-8
+"""
+图片上传下载
+"""
+import logging
+
+from flask import (
+    jsonify,
+    request,
+    send_file
+)
+
+from ...decorator import (
+    session_required
+)
+
+from ...error import (
+    Success,
+    Failed,
+    FailedLackOfField
+)
+from ....response import failure
+from ....service import (
+    AliOssService
+)
+
+logger = logging.getLogger(__name__)
+
+@session_required
+def upload_image_from_file():
+    """
+    直接上传图片到云盘
+    目前只用来传实名认证图片
+    """
+    image = request.files.get('image')
+    if not image:
+        return jsonify(Failed)
+
+    fileid = AliOssService.upload_image_from_binary(image)
+    if not fileid:
+        return jsonify(Failed)
+
+    return jsonify({
+        'success': True,
+        'data': {
+            'fileid': fileid
+        }
+    })
+
+def get_image(fileid):
+    content = AliOssService.get_binary_from_bucket(fileid)
+    if not content:
+        return jsonify(Failed)
+    return send_file(content, mimetype='image/jpeg')
