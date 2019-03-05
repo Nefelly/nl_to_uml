8978c79220163ed082d2312ac24acbbdf94d40ee
qingyi <qingyiwang@xiaohongshu.com>
2019-03-05 17:49:34 +0800

    add huanxin service

--- litatom/service/__init__.py
+++ litatom/service/__init__.py
@@ -2,3 +2,4 @@
 from .sms_code_service import SmsCodeService
 from .user_service import UserService
 from .ali_oss import AliOssService
+from .huanxin_service import HuanxinService
--- litatom/service/user_service.py
+++ litatom/service/user_service.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 import random
 
 from ..redis import RedisClient
