82cab0e599ca97c48b6a1abca9fe63fd55214fe2
qingyi <qingyiwang@xiaohongshu.com>
2019-03-08 16:52:07 +0800

    test

--- litatom/service/__init__.py
+++ litatom/service/__init__.py
@@ -1,10 +1,10 @@
 # flake8: noqa
 from .sms_code_service import SmsCodeService
-from .feed_service import FeedService
 from .huanxin_service import HuanxinService
 from .user_service import UserService
 from .ali_oss import AliOssService
 from .statistic_service import StatisticService
 from .follow_service import FollowService
 from .anoy_match_service import AnoyMatchService
+from .feed_service import FeedService
 from .debug_helper_service import DebugHelperService
