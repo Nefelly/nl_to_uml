139ecb1210e183016c3d346e7f26b22d4cfe8107
qingyi <qingyiwang@xiaohongshu.com>
2019-03-15 14:03:13 +0800

    test

--- scripts/pools_clear.py
+++ scripts/pools_clear.py
@@ -4,6 +4,6 @@ from litatom.service import AnoyMatchService
 
 
 if __name__ == "__main__":
-    print "started at", time.strftime("%Y-%m-%d %H:%M:%S", time.local_time(time.time()))
+    print "started at", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
     AnoyMatchService.clean_pools()
-    print "ended at", time.strftime("%Y-%m-%d %H:%M:%S", time.local_time(time.time()))
+    print "ended at", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
