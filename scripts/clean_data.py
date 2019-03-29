b2c5724d0e6a68cb868c1071f7a1d3120c82d285
qingyi <qingyiwang@xiaohongshu.com>
2019-03-29 20:31:29 +0800

    test

--- scripts/clean_data.py
+++ scripts/clean_data.py
@@ -1,4 +1,11 @@
 from litatom.model import *
+from litatom.redis import RedisClient
+
+redis_client = RedisClient()['lit']
+
+def clear_redis():
+    for k in redis_client.keys():
+        redis_client.delete(k)
 
 def clear_objs():
     for cls in [Blocked, Feed, FeedLike, FeedComment, Follow, Report, Feedback,
@@ -7,3 +14,4 @@ def clear_objs():
 
 if __name__ == '__main__':
     clear_objs()
+    clear_redis()
