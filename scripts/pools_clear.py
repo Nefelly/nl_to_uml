4f260de56ddc26229f2e2c4b3283886d089e62c9
qingyi <qingyiwang@xiaohongshu.com>
2019-03-29 20:18:15 +0800

    test

--- scripts/pools_clear.py
+++ scripts/pools_clear.py
@@ -1,15 +1,9 @@
 import os
 import time
 from litatom.service import AnoyMatchService
-from litatom.model import *
 
-def clear_objs():
-    for cls in [Blocked, Feed, FeedLike, FeedComment, Follow, Report, Feedback,
-                UserAction, UserRecord, UserMessage]:
-        cls.objects().delete()
 
 if __name__ == "__main__":
     print "started at", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
     AnoyMatchService.clean_pools()
     print "ended at", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
-    clear_objs()
