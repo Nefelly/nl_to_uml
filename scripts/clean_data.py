b5d5b5d1e521ed4beeffc49a68e1b1e2f6dd1423
qingyi <qingyiwang@xiaohongshu.com>
2019-03-29 20:39:36 +0800

    test

--- scripts/clean_data.py
+++ scripts/clean_data.py
@@ -1,5 +1,6 @@
 from litatom.model import *
 from litatom.redis import RedisClient
+from litatom.service import UserService
 
 redis_client = RedisClient()['lit']
 
@@ -8,6 +9,7 @@ def clear_redis():
         redis_client.delete(k)
 
 def clear_objs():
+    map(UserService._delete_user, User.objects())
     for cls in [Blocked, Feed, FeedLike, FeedComment, Follow, Report, Feedback,
                 UserAction, UserRecord, UserMessage]:
         cls.objects().delete()
