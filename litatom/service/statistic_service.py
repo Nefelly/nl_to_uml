455a9f9c4dd938e57879b3f1e03b72f4fae2c3fd
qingyi <qingyiwang@xiaohongshu.com>
2019-04-03 11:26:01 +0800

    test

--- litatom/service/statistic_service.py
+++ litatom/service/statistic_service.py
@@ -10,6 +10,7 @@ from ..const import (
     ONLINE_LIVE,
     GENDERS,
     GIRL,
+    BOY,
     MAX_TIME,
     USER_ACTIVE
 )
@@ -53,8 +54,18 @@ class StatisticService(object):
             key = REDIS_ONLINE_GENDER.format(gender=gender)
         else:
             key = REDIS_ONLINE
-        uids = redis_client.zrevrange(key, start_p, start_p + num)
-        has_uid = False
+
+        if gender == BOY and start_p == 0:
+            '''girls has to have some girl'''
+            b_ratio = 0.3
+            girl_num = int(num * b_ratio)
+            num = boy_num = int(num)   # set num to this for next get
+            girl_start_p = int(start_p * b_ratio) + 1
+            girl_uids = redis_client.zrevrange(REDIS_ONLINE_GENDER.format(gender=GIRL), girl_start_p, girl_start_p + girl_num)
+            boy_uids = redis_client.zrevrange(REDIS_ONLINE_GENDER.format(gender=BOY), start_p, start_p + boy_num)
+            uids = girl_uids + boy_uids
+        else:
+            uids = redis_client.zrevrange(key, start_p, start_p + num)
         temp_uid = request.user_id
         if temp_uid and temp_uid in uids:
             temp_num = num + 1
@@ -62,15 +73,23 @@ class StatisticService(object):
             uids = [uid for uid in uids if uid != temp_uid]
         uids = uids if uids else []
         has_next = False
-        if len(uids) == num + 1:
-            has_next = True
-            uids = uids[:-1]
+        if gender == BOY:
+            if len(boy_uids) == num + 1:
+                has_next = True
+                uids = boy_uids[:-1] + boy_uids
+        else:
+            if len(uids) == num + 1:
+                has_next = True
+                uids = uids[:-1]
         user_infos = []
         if uids:
-            all_online = cls.uid_online(key, uids[-1]) == True   # last user online
-            all_not_online = cls.uid_online(key, uids[0]) == False   # first user not online
+            if gender == BOY:
+                ''' has to cal onlines on every person'''
+                all_online = all_not_online = False
+            else:
+                all_online = cls.uid_online(key, uids[-1]) == True   # last user online
+                all_not_online = cls.uid_online(key, uids[0]) == False   # first user not online
             for uid in uids:
-
                 _ = UserService.get_basic_info(User.get_by_id(uid))
                 if all_online:
                     online = True
--- scripts/clean_data.py
+++ scripts/clean_data.py
@@ -1,12 +1,29 @@
 from litatom.model import *
 from litatom.redis import RedisClient
 from litatom.service import UserService
+import datetime
+import time
 
 redis_client = RedisClient()['lit']
 
 def clear_redis():
     for k in redis_client.keys():
         redis_client.delete(k)
+
+def clear_by_date():
+    time.mktime(time.strptime("2019-04-02 00:00:00","%Y-%m-%d %H:%M:%S"))
+    a = datetime.datetime(2019, 4,2, 0, 0 ,0)
+    ls = User.objects(create_time__lte=a)
+    ls = list(ls)
+    map(UserService._delete_user, ls)
+
+def clear_feeds():
+    a = time.mktime(time.strptime("2019-04-02 00:00:00","%Y-%m-%d %H:%M:%S"))
+    ls = redis_client.zrangebyscore('feed_square', 0, a)
+    def r(fid):
+        redis_client.zrem('feed_square', fid)
+        Feed.get_by_id(fid).delete()
+    map(r, ls)
 
 def clear_objs():
     map(UserService._delete_user, User.objects())
@@ -15,5 +32,7 @@ def clear_objs():
         cls.objects().delete()
 
 if __name__ == '__main__':
+    exit()
+    assert False
     clear_objs()
     clear_redis()
