9711a0cb05148014cf5af333c05d7b0aa89401c1
qingyi <qingyiwang@xiaohongshu.com>
2019-04-11 21:06:39 +0800

    test

--- litatom/service/qiniu_service.py
+++ litatom/service/qiniu_service.py
@@ -13,12 +13,14 @@ class QiniuService(object):
     AK = "KRI2AE8Cedn4hmtTQQnS9RjVhXNJEyvx2MCAbLS3"
     SK = "aDQkhwOsRKwXgAFPSRcMtVoNT5F1UolZECaPIBCm"
     AUTH = QiniuMacAuth(AK, SK)
+    JUDGE_SCORE = 0.93
+
     '''
     docs :https://developer.qiniu.com/censor/api/5588/image-censor
     '''
 
     @classmethod
-    def check_pic_from_url(cls, url):
+    def should_pic_block_from_url(cls, url):
         data = {
             "data": {
                 "uri": url
@@ -34,15 +36,18 @@ class QiniuService(object):
         }
         url = 'http://ai.qiniuapi.com/v3/image/censor'
         for i in range(3):
-
             try:
                 ret, res = http._post_with_qiniu_mac(url, data, cls.AUTH)
-                headers = {"code": res.status_code, "reqid": res.req_id, "xlog": res.x_log}
-                print json.dumps(headers, indent=4, ensure_ascii=False)
-                print res
-                return True
+                # headers = {"code": res.status_code, "reqid": res.req_id, "xlog": res.x_log}
+                test_res = res.text_body
+                scenes = test_res['result']['scenes']
+                for r in scenes:
+                    details = scenes[r].get('details', {})
+                    if details and details[0]['score'] > cls.JUDGE_SCORE:
+                        return r
+                return ''
             except Exception, e:
                 traceback.print_exc()
-                logger.error('Error block Qiniu, user_id: %r, err: %r', source_user_name, e)
-                return {}
-        return False
+                logger.error('Error verify Qiniu, url: %r, err: %r', url, e)
+
+        return ''
