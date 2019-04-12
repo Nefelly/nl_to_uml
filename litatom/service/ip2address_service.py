29712711bd0194855ff93562f0afe2ab4234714c
qingyi <qingyiwang@xiaohongshu.com>
2019-04-12 11:57:10 +0800

    test

--- litatom/service/ip2address_service.py
+++ litatom/service/ip2address_service.py
@@ -6,8 +6,6 @@ import sys
 
 logger = logging.getLogger(__name__)
 
-sys.setdefaultencoding('utf-8')
-
 
 class Ip2AddressService(object):
     READER = geoip2.database.Reader('litatom/data/GeoLite2-City.mmdb')
@@ -19,27 +17,27 @@ class Ip2AddressService(object):
     def get_ip_all_info(cls, ip):
         response = cls.READER.city(ip)
         # 有多种语言，我们这里主要输出英文和中文
-        print("你查询的IP的地理位置是:")
+        print(u"你查询的IP的地理位置是:")
 
-        print("地区：{}({})".format(response.continent.names["es"],
+        print(u"地区：{}({})".format(response.continent.names["es"],
                                  response.continent.names["zh-CN"]))
 
-        print("国家：{}({}) ，简称:{}".format(response.country.name,
+        print(u"国家：{}({}) ，简称:{}".format(response.country.name,
                                         response.country.names["zh-CN"],
                                         response.country.iso_code))
 
-        print("洲／省：{}({})".format(response.subdivisions.most_specific.name,
+        print(u"洲／省：{}({})".format(response.subdivisions.most_specific.name,
                                   response.subdivisions.most_specific.names["zh-CN"]))
 
-        print("城市：{}({})".format(response.city.name,
+        print(u"城市：{}({})".format(response.city.name,
                                  response.city.names["zh-CN"]))
 
-        print("经度：{}，纬度{}".format(response.location.longitude,
+        print(u"经度：{}，纬度{}".format(response.location.longitude,
                                   response.location.latitude))
 
-        print("时区：{}".format(response.location.time_zone))
+        print(u"时区：{}".format(response.location.time_zone))
 
-        print("邮编:{}".format(response.postal.code))
+        print(u"邮编:{}".format(response.postal.code))
 
     @classmethod
     def ip_country(cls, ip):
