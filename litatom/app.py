92e1c8d90dfa7d1dd40760eb13f869627e031ef7
qingyi <qingyiwang@xiaohongshu.com>
2019-04-26 23:56:14 +0800

    test

--- litatom/app.py
+++ litatom/app.py
@@ -80,7 +80,6 @@ class LitatomAppFactory(object):
         name = app.name
         if not cls._logging_setup:
             conf = log.gen_app_log_config(name, app.debug)
-            print conf
             logging.config.dictConfig(conf)
             logging.captureWarnings(True)
             cls._logging_setup = True
