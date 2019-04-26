79a4a048bb98864494ddc87b0d3f2185d8541288
qingyi <qingyiwang@xiaohongshu.com>
2019-04-26 15:14:49 +0800

    test

--- litatom/app.py
+++ litatom/app.py
@@ -169,8 +169,8 @@ class PathDispatchMiddleware(object):
             app = self.get_app(environ)
             return app(environ, start_response)
         except Exception as e:
-            import traceback
-            traceback.print_exc()
+            #import traceback
+            #traceback.print_exc()
             logger.error(str(e), exc_info=True)
 
 
