5a89cf6d835542e84457946b7e118c06cb86c47d
qingyi <qingyiwang@xiaohongshu.com>
2019-04-26 22:19:37 +0800

    test

--- litatom/api/v1/endpoint/test.py
+++ litatom/api/v1/endpoint/test.py
@@ -14,13 +14,13 @@ from flask import (
 from ...error import Success
 
 logger = logging.getLogger(__name__)
-loghanlder = logging.FileHandler("/rdata/devlog", encoding='utf-8')
-logger.addHandler(loghanlder)
+#loghanlder = logging.FileHandler("/rdata/devlog", encoding='utf-8')
+#logger.addHandler(loghanlder)
 
 def test():
     return jsonify(Success)
 
 def hello():
-    assert False
-    logger.info("hello, this is a mistake")
+    #assert False
+    logger.error("hello, this is a mistake")
     return render_template('hello.html', name='joey')
