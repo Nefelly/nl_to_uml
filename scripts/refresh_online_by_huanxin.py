54ee831a943522874a3b05b6d5f3768df25d8d5e
qingyi <qingyiwang@xiaohongshu.com>
2019-05-10 14:34:30 +0800

    test

--- scripts/refresh_online_by_huanxin.py
+++ scripts/refresh_online_by_huanxin.py
@@ -12,13 +12,14 @@ from litatom.key import (
 redis_client = RedisClient()['lit']
 
 def refresh_online():
-    scan_range = 10
+    scan_range = 100
     time_now = int(time.time())
     for g in GENDERS:
         key = REDIS_ONLINE_GENDER.format(gender=g)
         start_scan_time = time_now + USER_ACTIVE - scan_range
         end_scan_time = time_now + USER_ACTIVE + scan_range
         uids = redis_client.zrangebyscore(key, start_scan_time, end_scan_time)
+        print 'uids to scan', uids
         uid_online_m = UserService.uid_online_by_huanxin(uids)
         for uid, status in uid_online_m.items():
             if status:
