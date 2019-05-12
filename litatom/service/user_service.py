a32add20e2057a19df3778590d4c318773ce85b3
qingyi <qingyiwang@xiaohongshu.com>
2019-05-12 18:50:47 +0800

    test

--- litatom/service/user_service.py
+++ litatom/service/user_service.py
@@ -111,7 +111,8 @@ class UserService(object):
         # for _ in User.objects():
         #     if _.huanxin.user_id:
         #         huanxin_ids.append(_.huanxin.user_id)
-        huanxin_ids = [u'5ca4b28f3fff2239491354b3']   # joey
+        huanxin_ids = [u'love123879348711830']   # joey
+
         HuanxinService.batch_send_msgs(msg, huanxin_ids, officail_user.huanxin.user_id)
         return True
 
