67aa15998624567e21542486b92245e2c0e99a59
qingyi <qingyiwang@xiaohongshu.com>
2019-06-05 20:00:47 +0800

    test

---
+++ scripts/clear_region.py
@@ -0,0 +1,22 @@
+import os
+import time
+from litatom.service import AnoyMatchService, VoiceMatchService, GlobalizationService
+from litatom.user_service import *
+
+
+def clear():
+    th = [e for e in redis_client.zscan_iter('online:th')]
+    id = [e for e in redis_client.zscan_iter('online:id')]
+    vn = [e for e in redis_client.zscan_iter('online:vn')]
+    o = id + vn
+    m = {}
+    for k, v in th:
+        m[k]= v
+    to_clear = [el[0] for el in o if el[0] not in m]
+
+
+if __name__ == "__main__":
+    print "started at", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
+    AnoyMatchService.clean_pools()
+    VoiceMatchService.clean_pools()
+    print "ended at", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
