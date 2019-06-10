8ed793a97e3236640e9386d318b0d86668bf8abc
qingyi <qingyiwang@xiaohongshu.com>
2019-06-10 17:59:58 +0800

    test

---
+++ litatom/model/palm.py
@@ -0,0 +1,39 @@
+# coding: utf-8
+import datetime
+import json
+import bson
+from mongoengine import (
+    BooleanField,
+    DateTimeField,
+    Document,
+    IntField,
+    ListField,
+    StringField,
+)
+
+
+class PalmResult(Document):
+    meta = {
+        'strict': False,
+        'alias': 'db_alias'
+    }
+
+    result = StringField(required=True)
+    create_time = DateTimeField(required=True, default=datetime.datetime.now)
+
+    @classmethod
+    def create(cls, result):
+        obj = cls()
+        obj.result = json.dumps(result)
+        obj.save()
+        return str(obj.id)
+
+    @classmethod
+    def get_by_id(cls, result_id):
+        if not bson.ObjectId.is_valid(result_id):
+            return None
+        obj = cls.objects(id=result_id).first()
+        if obj:
+            return json.loads(obj.result)
+        return {}
+
