7002e112c9ded3eeddb1b545732ed8f5e52db703
qingyi <qingyiwang@xiaohongshu.com>
2019-06-11 15:28:45 +0800

    test

---
+++ litatom/model/message.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+import datetime
+import json
+import bson
+from mongoengine import (
+    BooleanField,
+    DateTimeField,
+    Document,
+    IntField,
+    ListField,
+    StringField,
+)
+
+
+class HuanxinMessage(Document):
+    meta = {
+        'strict': False,
+        'alias': 'huanxin_message'
+    }
+
+    result = StringField(required=True)
+    user_id = StringField(required=True)
+    create_time = DateTimeField(required=True, default=datetime.datetime.now)
+
+    @classmethod
+    def create(cls, result, user_id):
+        obj = cls()
+        obj.result = json.dumps(result)
+        obj.user_id = user_id
+        obj.save()
+        return str(obj.id)
+
+    @classmethod
+    def get_by_id(cls, result_id):
+        if not bson.ObjectId.is_valid(result_id):
+            return None
+        obj = cls.objects(id=result_id).first()
+        if obj:
+            return json.loads(obj.result)
+        return {}
+
