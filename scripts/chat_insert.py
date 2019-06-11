4f916dc8d207405446d2d9294cb028d7d6d5f449
qingyi <qingyiwang@xiaohongshu.com>
2019-06-11 19:12:56 +0800

    test

--- scripts/chat_insert.py
+++ scripts/chat_insert.py
@@ -19,7 +19,7 @@ def insert_records():
     time_now = time.time()
     two_hour_ago = ChatRecordService.get_hour_str(time_now - 3 * 3600)
     content = ChatRecordService.get_source_content(two_hour_ago)
-    path = os.path.join(save_dir, '%s/%s/%s.txt'% (two_hour_ago[:6], two_hour_ago[6:], two_hour_ago))
+    path = os.path.join(save_dir, '%s/%s.txt'% (two_hour_ago[:6], two_hour_ago))
     ensure_path(path)
     if os.path.exists(path):
         return
