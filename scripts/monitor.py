2bc8775700b0d0dffae374a10be2142faf947e53
qingyi <qingyiwang@xiaohongshu.com>
2019-07-02 17:35:37 +0800

    test

--- scripts/monitor.py
+++ scripts/monitor.py
@@ -7,7 +7,12 @@ def monitor_mongo():
         os.popen('cp /data0/mongodb_data/logs/mongodb.log /data0/mongodb_data/logs/mongodb.log.bak')
         os.popen('/opt/mongodb/bin/mongod --config /opt/mongodb/bin/mongodb.conf>/data0/log')
 
+def monitor_logstash():
+    y = os.popen('ps axu|grep logstash|grep conf|grep -v ps|awk \'{print $1}\'')
+    if not y.read():
+        os.popen('cd /usr/local/src/logstash-6.2.4; ./bin/logstash -f config/logstash.conf 2>&1 &')
 
 if __name__ == "__main__":
     monitor_mongo()
+    monitor_logstash()
     #rem()
