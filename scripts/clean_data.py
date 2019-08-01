db5f62f29247a365229c6d6f9947e31c8305ca6f
qingyi <qingyiwang@xiaohongshu.com>
2019-08-01 23:27:43 +0800

    test

--- scripts/clean_data.py
+++ scripts/clean_data.py
@@ -12,14 +12,14 @@ def clear_redis():
         redis_client.delete(k)
 
 def clear_by_date():
-    time.mktime(time.strptime("2019-04-02 00:00:00","%Y-%m-%d %H:%M:%S"))
+    time.mktime(time.strptime("2019-11-02 00:00:00","%Y-%m-%d %H:%M:%S"))
     a = datetime.datetime(2019, 4,2, 0, 0 ,0)
     ls = User.objects(create_time__lte=a)
     ls = list(ls)
     map(UserService._delete_user, ls)
 
 def clear_feeds():
-    a = time.mktime(time.strptime("2019-04-02 00:00:00","%Y-%m-%d %H:%M:%S"))
+    a = time.mktime(time.strptime("2019-11-02 00:00:00","%Y-%m-%d %H:%M:%S"))
     ls = redis_client.zrangebyscore('feed_square', 0, a)
     def r(fid):
         redis_client.zrem('feed_square', fid)
