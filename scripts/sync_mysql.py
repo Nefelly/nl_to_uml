0bf8ebd264da2f2cf7dbd92b32b183b55cfed96d
qingyi <qingyiwang@xiaohongshu.com>
2019-08-19 00:33:51 +0800

    test

---
+++ scripts/sync_mysql.py
@@ -0,0 +1,23 @@
+import os
+import time
+from litatom.service.mysql_sync_service import *
+from hendrix.conf import setting
+
+save_dir = '/data/datas/mysql'
+
+def run():
+    mutex_f = '/var/run/%s.mutex' % __file__.split('/')[-1].replace('.py', '')
+    if setting.IS_DEV:
+        mutex_f += 'dev'
+    f = open(mutex_f, 'w')
+    try:
+        fcntl.flock(f,fcntl.LOCK_EX|fcntl.LOCK_NB)
+    except:
+        print 'program already in run'
+        sys.exit(0)
+    MysqlSyncService.run_all()
+
+if __name__ == "__main__":
+    print "started at", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
+    run()
+    print "ended at", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
