723613db9836ffbc8b2383fd8eb18c2d04fbcc3d
qingyi <qingyiwang@xiaohongshu.com>
2019-08-19 00:42:39 +0800

    test

--- litatom/service/mysql_sync_service.py
+++ litatom/service/mysql_sync_service.py
@@ -254,5 +254,5 @@ class MysqlSyncService(object):
     def c(cls):
         print dir(model)
 
-# print MysqlSyncService.create_table(UserAddressList)
-# print MysqlSyncService.update_tb(UserAddressList)
+print MysqlSyncService.create_table(UserAddressList)
+print MysqlSyncService.update_tb(UserAddressList)
