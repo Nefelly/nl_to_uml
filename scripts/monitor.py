7a129f34ca74df9d116b76e17164272d6de0c29c
qingyi <qingyiwang@xiaohongshu.com>
2019-08-22 17:09:42 +0800

    test

--- scripts/monitor.py
+++ scripts/monitor.py
@@ -13,6 +13,8 @@ def monitor_logstash():
         os.popen('cd /usr/local/src/logstash-6.2.4; ./bin/logstash -f config/logstash.conf 2>&1 &')
 
 if __name__ == "__main__":
-    monitor_mongo()
-    monitor_logstash()
+    for i in range(100):
+        monitor_mongo()
+        monitor_logstash()
+        time.sleep(1)
     #rem()
