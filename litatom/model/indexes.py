2658f9fd91a29eec9e3215caaa51149eca0313f1
qingyi <qingyiwang@xiaohongshu.com>
2019-08-22 18:08:20 +0800

    test

--- litatom/model/indexes.py
+++ litatom/model/indexes.py
@@ -1,20 +1,20 @@
 indexes = {
     "admin_user": ["user_name"],
-    "blocked": ["uid", "blocked"],
+    "blocked": ["uid", "blocked", "create_time"],
     "feed":["user_id", "create_time"],
-    "feed_like":["feed_id", "uid"],
-    "feed_comment": ["feed_id", "comment_id"],
-    "feedback": ["uid"],
-    "firebase_info": ["user_id", "user_token"],
-    "follow": ["uid", "followed"],
-    "report": ["uid", "target_uid", "deal_user"],
+    "feed_like":["feed_id", "uid", "create_time"],
+    "feed_comment": ["feed_id", "comment_id", "create_time"],
+    "feedback": ["uid", "create_ts"],
+    "firebase_info": ["user_id", "user_token", "create_time"],
+    "follow": ["uid", "followed", "create_time"],
+    "report": ["uid", "target_uid", "deal_user", "create_ts"],
     "track_chat": ["uid", "target_uid", "create_ts"],
     "user_message": ["uid", "related_uid", "create_time"],
-    "user": ["phone", "huanxin.user_id", "google.other_id", "facebook.other_id", "nickname", "session"],
-    "user_setting": ["user_id"],
+    "user": ["phone", "huanxin.user_id", "google.other_id", "facebook.other_id", "nickname", "session", "create_time"],
+    "user_setting": ["user_id", "create_time"],
     "user_action": ["user_id", "create_time"],
     "user_record": ["user_id", "create_time"],
-    "FollowingFeed": ["user_id", "followed_user_id", "feed_create_time"]
+    "FollowingFeed": ["user_id", "followed_user_id", "feed_create_time", "create_time"]
 }
 
 if __name__ == "__main__":
