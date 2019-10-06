e0431987b519bf6fd5d10d2a931a1be887ee21d3
joeyqy wang <hypercycle@litatom.com>
2019-10-06 23:38:22 +0800

    test

--- litatom/service/voice_match_service.py
+++ litatom/service/voice_match_service.py
@@ -51,7 +51,7 @@ class VoiceMatchService(object):
     MATCH_INT = 60 * 7  # talking time
     TOTAL_WAIT = MATCH_INT + MATCH_WAIT + FIVE_MINS
     MAX_CHOOSE_NUM = 100
-    MATCH_TMS = 1000 if not setting.IS_DEV else 1000
+    MATCH_TMS = 5 if not setting.IS_DEV else 1000
     OTHER_GENDER_M = {BOY: GIRL, GIRL: BOY}
 
     @classmethod
