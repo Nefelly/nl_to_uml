2e2a8c348cb9c8e12ee58c0e3eb4b02e2faa01c4
joeyqy wang <hypercycle@litatom.com>
2019-10-06 23:21:47 +0800

    test

--- litatom/service/anoy_match_service.py
+++ litatom/service/anoy_match_service.py
@@ -56,7 +56,7 @@ class AnoyMatchService(object):
     MATCH_INT = 60 * 3  # talking time
     TOTAL_WAIT = MATCH_INT + MATCH_WAIT + FIVE_MINS
     MAX_CHOOSE_NUM = 100
-    MATCH_TMS = 20 if not setting.IS_DEV else 1000
+    MATCH_TMS = 1000 if not setting.IS_DEV else 1000
     OTHER_GENDER_M = {BOY: GIRL, GIRL: BOY}
 
     @classmethod
