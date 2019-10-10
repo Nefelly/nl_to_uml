3bf28c78926601ecc857a2126c69d5068e2a6f42
joeyqy wang <hypercycle@litatom.com>
2019-10-10 12:58:14 +0800

    test

--- newStart.sh
+++ newStart.sh
@@ -5,6 +5,5 @@ exec 2>&1
 git pull
 sh entrypoint.sh
 ps axu|grep runsvdir|grep /etc/service|awk '{print $2}'|xargs kill -9
-runsvdir -P /etc/service 2>&1 &
 sleep 1
-sv restart litatom
+runsvdir -P /etc/service 2>&1 &
