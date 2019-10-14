0f34a2d7042df0863a1a989e55936461463431a8
joeyqy wang <hypercycle@litatom.com>
2019-10-14 18:21:49 +0800

    test

--- litatom/service/__init__.py
+++ litatom/service/__init__.py
@@ -1,6 +1,7 @@
 # flake8: noqa
 from .token_bucket_service import TokenBucketService
 from .ip2address_service import Ip2AddressService
+from .youtube_service import YoutubeService
 from .alert_service import AlertService
 from .globalization_service import GlobalizationService
 from .mq_service import MqService
