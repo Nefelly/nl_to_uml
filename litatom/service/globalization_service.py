88f01729b1eabeee7607a20ab6db34faacaad30e
joeyqy wang <hypercycle@litatom.com>
2019-10-18 15:00:23 +0800

    test

--- litatom/service/globalization_service.py
+++ litatom/service/globalization_service.py
@@ -117,7 +117,7 @@ class GlobalizationService(object):
     todo: user loc set in redis
     '''
     @classmethod
-    def _online_key_by_region_gender(cls, gender=None):LOC_KR
+    def _online_key_by_region_gender(cls, gender=None):
         region = cls.get_region()
         if gender:
             return REDIS_ONLINE_GENDER_REGION.format(region=region, gender=gender)
