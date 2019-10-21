56cf5d7d2760e6e0b612e8a566a90961df490e7e
joeyqy wang <hypercycle@litatom.com>
2019-10-21 16:08:55 +0800

    test

--- litatom/service/user_service.py
+++ litatom/service/user_service.py
@@ -530,7 +530,7 @@ class UserService(object):
         for _ in uids:
             pp = redis_client.pipeline()
             pp.zscore(key, _)
-        print uids
+        print pp.execute()
         for uid, score in zip(uids, pp.execute()):
             print uid, score
             if not score or int(score) < judge_time:
