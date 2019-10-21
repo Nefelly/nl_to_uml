28e097786beeee91596d274aa125eaef0010d6e2
joeyqy wang <hypercycle@litatom.com>
2019-10-21 21:47:58 +0800

    test

--- scripts/user_action_back.py
+++ scripts/user_action_back.py
@@ -1,53 +1,39 @@
 import os
 import time
 import datetime
+from hendrix.conf import setting
 from litatom.service import ChatRecordService
 
 keep_time = 8 * 3600 * 24
-save_dir = '/data/chats'
-
-def back():
-    host = '172.31.138.46'
-    port = 27017
-    user = "ll"
-    pwd = "11"
-    query_time = int(time.time()) - keep_time
-    save_add = "%s/%s" % (save_dir, (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y-%m-%d'))
-    sql = '''mongoexport -h %s --port %r -u %s -p %s --authenticationDatabase lit -d lit -c user_action -o %s -q '{"create_time": {$lt:%s}}}''' % (host, port, user, pwd, save_add, query_time)
-    os.system(sql)
-
+save_dir = '/data/mongoback/user_action'
 
 def ensure_path(path):
     dir_name = path[:path.rfind('/')]
     if not os.path.exists(dir_name):
         os.makedirs(dir_name)
 
+def back():
+    db_m = setting.DB_SETTINGS.get('DB_LIT')
+    ''''host': u'mongodb://ll:11@172.31.138.46/lit?authsource=lit','''
+    port = db_m['port']
+    conn_url = db_m['host']
+    user_pwd = conn_url.split('://')[1].split('@')[0]
+    user, pwd = user_pwd.split(':')
+    host = conn_url.split('@')[1].split('/')[0]
+    query_time = int(time.time()) - keep_time
+    save_add = "%s/%s" % (save_dir, (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y-%m-%d'))
+    ensure_path(save_add)
+    sql = '''mongoexport -h %s --port %r -u %s -p %s --authenticationDatabase lit -d lit -c user_action -o %s -q '{"create_time": {$lt:%s}}}''' % (host, port, user, pwd, save_add, query_time)
+    print sql
+    # os.popen(sql)
+
+
 def clean_old_records():
     time_now = int(time.time())
-    HuanxinMessage.objects(create_time__lt=(time_now - keep_time))
 
-def insert_records():
-    time_now = time.time()
-    two_hour_ago = ChatRecordService.get_hour_str(time_now - 2 * 3600)
-    content = ChatRecordService.get_source_content(two_hour_ago)
-    path = os.path.join(save_dir, '%s/%s.txt'% (two_hour_ago[:6], two_hour_ago))
-    ensure_path(path)
-    if os.path.exists(path):
-        return
-    with open(path, 'w') as f:
-        f.write(content)
-        f.close()
-    msgs = ChatRecordService.records_by_content(content)
-    for msg in msgs:
-        try:
-            ChatRecordService.save_to_db(msg)
-        except Exception as e:
-            print e
-            continue
 
 
 if __name__ == "__main__":
     print "started at", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
-    clean_old_records()
-    insert_records()
+    back()
     print "ended at", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
