0fc9b82b77c916d7ccad1319030b21fb78650e26
joeyqy wang <hypercycle@litatom.com>
2019-10-21 22:00:59 +0800

    test

--- scripts/user_action_back.py
+++ scripts/user_action_back.py
@@ -2,10 +2,11 @@ import os
 import time
 import datetime
 from hendrix.conf import setting
-from litatom.service import ChatRecordService
+from litatom.model import UserAction
 
 keep_time = 8 * 3600 * 24
 save_dir = '/data/mongoback/user_action'
+query_time = int(time.time()) - keep_time
 
 def ensure_path(path):
     dir_name = path[:path.rfind('/')]
@@ -20,20 +21,22 @@ def back():
     user_pwd = conn_url.split('://')[1].split('@')[0]
     user, pwd = user_pwd.split(':')
     host = conn_url.split('@')[1].split('/')[0]
+    global query_time
     query_time = int(time.time()) - keep_time
     save_add = "%s/%s" % (save_dir, (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y-%m-%d'))
     ensure_path(save_add)
     sql = '''mongoexport -h %s --port %r -u %s -p %s --authenticationDatabase lit -d lit -c user_action -o %s -q '{"create_time": {$lt:%s}}\'''' % (host, port, user, pwd, save_add, query_time)
     print sql
-    # os.popen(sql)
+    os.popen(sql)
 
 
 def clean_old_records():
-    time_now = int(time.time())
-
+    global query_time
+    UserAction.objects(create_time__lte=query_time).delete()
 
 
 if __name__ == "__main__":
     print "started at", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
     back()
+    clean_old_records()
     print "ended at", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
