a2666b912cbf0b10d8715687b3d7cc8d86a68397
joeyqy wang <hypercycle@litatom.com>
2019-10-21 16:11:15 +0800

    test

--- litatom/service/user_service.py
+++ litatom/service/user_service.py
@@ -531,7 +531,6 @@ class UserService(object):
         for _ in uids:
             pp.zscore(key, _)
         for uid, score in zip(uids, pp.execute()):
-            print uid, score
             if not score or int(score) < judge_time:
                 res[uid] = False
             else:
