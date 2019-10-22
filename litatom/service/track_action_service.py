451c455715e5fdbc41bdff940b86bca18db60de7
joeyqy wang <hypercycle@litatom.com>
2019-10-22 21:12:24 +0800

    test

--- litatom/service/track_action_service.py
+++ litatom/service/track_action_service.py
@@ -36,7 +36,9 @@ class TrackActionService(object):
     def pymongo_batch_insert(cls, client, payload_list):
         insert_pack = []
         for el in payload_list:
+            print el
             lst = cPickle.loads(el.get('args'))
+            print lst
             user_id, action, other_user_id, amount, remark, create_date, create_time = tuple(lst)
             # user_id = el.get('user_id')
             # action = el.get('action')
