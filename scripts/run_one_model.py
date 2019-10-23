2f5a54ff571a2fd6013c5b4c67410441ac61ce1b
joeyqy wang <hypercycle@litatom.com>
2019-10-23 19:37:55 +0800

    test

---
+++ scripts/run_one_model.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import fcntl
+from hendrix.conf import setting
+from litatom.service import AlertService
+import time
+
+
+def foo():
+    pass
+
+def run():
+    mutex_f = '/var/run/%s.mutex' % __file__.split('/')[-1].replace('.py', '')
+    if setting.IS_DEV:
+        mutex_f += 'dev'
+    f = open(mutex_f, 'w')
+    try:
+        fcntl.flock(f, fcntl.LOCK_EX | fcntl.LOCK_NB)
+    except:
+        print 'program already in run'
+        sys.exit(0)
+    while (True):
+        foo()
+        time.sleep(1)
+
+if __name__ == "__main__":
+    run()
+
