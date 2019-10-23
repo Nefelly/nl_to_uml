99a61a291eff1578ecc13dc6a5dc1fe9744cc4c2
joeyqy wang <hypercycle@litatom.com>
2019-10-23 23:19:55 +0800

    test

--- litatom/service/retain_stat_service.py
+++ litatom/service/retain_stat_service.py
@@ -51,11 +51,14 @@ class RetainStatService(object):
     def start_match(cls, uids, start_d):
         end_d = next_date(start_d)
         m = {}
+        tt = 0
         for uid in uids:
-            if UserAction.objects(user_id=uid, create_time__gte=date_to_int_time(start_d),
-                                  create_time__lte=date_to_int_time(end_d), remark__contains='start').count() > 0:
+            cnt = UserAction.objects(user_id=uid, create_time__gte=date_to_int_time(start_d), create_time__lte=date_to_int_time(end_d),
+                                     remark__contains='start').count()
+            if cnt > 0:
                 m[uid] = 1
-        return len(m.keys())
+                tt += cnt
+        return len(m.keys()), m, tt
 
     @classmethod
     def stat_match_succ(cls, uids, start_d):
@@ -72,6 +75,24 @@ class RetainStatService(object):
             else:
                 m[uid] += 1
         return len(m.keys()), m
+
+    @classmethod
+    def test_all(cls, d='20191021'):
+        start_d = get_times_from_str(d)
+        m, l = cls.register_userids(start_d)
+        print "total", l
+        s_match_num, s_m, sm_cnt = RetainStatService.start_match(m, start_d)
+        print "started match:%r, match_times:%r" % (s_match_num, sm_cnt)
+        succ_num, succ_uids = cls.stat_match_succ(m, start_d)
+        print "match success uids:", succ_num
+        not_match_succ = {}
+        for _ in s_m:
+            if _ not in succ_uids:
+                not_match_succ[_] = 1
+        print "match and fail num", len(not_match_succ.keys())
+        nomsucc_ac, nomsuccnum = cls.stat_active(not_match_succ, next_date(start_d))
+        print "match and fail active next day", nomsucc_ac, "actions num", nomsuccnum
+
 
     @classmethod
     def stat_active(cls, uids, start_d):
