236ef8660bad5e6453e305fc87d9019e037f811f
joeyqy wang <hypercycle@litatom.com>
2019-10-26 16:06:20 +0800

    add home page

--- litatom/app.py
+++ litatom/app.py
@@ -138,7 +138,7 @@ def create_app_by_prefix(prefix):
     elif prefix == 'api':
         return LitatomAppFactory.create_app(route_gen=default_api_blueprints)
     else:
-        return LitatomAppFactory.create_app(route_gen=default_api_blueprints)
+        return LitatomAppFactory.create_app(route_gen=home_api_blue_prints)
 
 
 class PathDispatchMiddleware(object):
