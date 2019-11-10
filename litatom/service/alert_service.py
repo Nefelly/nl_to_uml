5c2e6ab232c125ce6589d81153285d157c3eb06c
joeyqy wang <hypercycle@litatom.com>
2019-11-10 21:07:54 +0800

    test

--- litatom/service/alert_service.py
+++ litatom/service/alert_service.py
@@ -4,6 +4,7 @@ import logging
 import json
 import smtplib
 from email.mime.text import MIMEText  # 邮件文本
+from email.mime.multipart import MIMEMultipart
 
 
 logger = logging.getLogger(__name__)
@@ -19,6 +20,26 @@ class AlertService(object):
         sender = '382365209@qq.com'
         pwd = 'vshditifewnhcajb'
         message = MIMEText(content, "plain", "utf-8")
+        message['Subject'] = 'alert' if not subject else subject
+        if not isinstance(to, list):
+            to = [to]
+        message['To'] = ','.join(to)
+        message['From'] = sender
+        smtp = smtplib.SMTP_SSL("smtp.qq.com", 465)
+        smtp.login(sender, pwd)
+        smtp.sendmail(sender, to, message.as_string())
+
+    @classmethod
+    def send_file(cls, to, file_add, subject='stat'):
+        message = MIMEMultipart()
+
+        att2 = MIMEText(open(file_add, 'rb').read(), 'base64', 'utf-8')
+        att2["Content-Type"] = 'application/octet-stream'
+        att2["Content-Disposition"] = 'attachment; filename="%s"' % file_add
+        message.attach(att2)
+
+        sender = '382365209@qq.com'
+        pwd = 'vshditifewnhcajb'
         message['Subject'] = 'alert' if not subject else subject
         if not isinstance(to, list):
             to = [to]
