f292d0ce8539ae87dd52fbca9ac37d996b33ac72
joeyqy wang <hypercycle@litatom.com>
2019-11-25 12:51:58 +0800

    test

--- litatom/api/v1/endpoint/home.py
+++ litatom/api/v1/endpoint/home.py
@@ -119,7 +119,7 @@ def settings():
     return success(UserSettingService.get_settings(request.user_id))
 
 def check_version():
-    version_now = '2.5.1'
+    version_now = '2.5.6'
     version = request.args.get('version', None)
     if 0 and GlobalizationService.get_region() == GlobalizationService.REGION_TH:
         message = u'กรุณาอัพเดทเวอร์ชั่น เราได้ทำการแก้ไขปัญหาส่งข้อความเรียบร้อยแล้ว ขอบคุณค่ะ'
