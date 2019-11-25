dfe7ac212036b5904d33e58245d6fe40c26e891a
joeyqy wang <hypercycle@litatom.com>
2019-11-25 12:52:51 +0800

    test

--- litatom/api/v1/endpoint/home.py
+++ litatom/api/v1/endpoint/home.py
@@ -124,7 +124,7 @@ def check_version():
     if 0 and GlobalizationService.get_region() == GlobalizationService.REGION_TH:
         message = u'กรุณาอัพเดทเวอร์ชั่น เราได้ทำการแก้ไขปัญหาส่งข้อความเรียบร้อยแล้ว ขอบคุณค่ะ'
     else:
-        message = u'New Update: Big update.'
+        message = u'add friends function'
     if version < version_now:
         data = {
             'need_update': True,
