ab61fdcf00437c9e6e240e34f1dc56ec1aef366c
joeyqy wang <hypercycle@litatom.com>
2019-11-27 17:09:18 +0800

    test

--- litatom/service/admin_service.py
+++ litatom/service/admin_service.py
@@ -201,7 +201,7 @@ class AdminService(object):
         sql = '''mongoexport -h %s --port %r -u %s -p %s --authenticationDatabase %s -d %s -c user_action -f %s --type=csv -q '%s' --out %s''' % (
         host, port, user, pwd, db, db, fields, query, save_add)
         print sql
-        os.popen(sql)
+        os.system(sql)
         return save_add, True
 
     @classmethod
