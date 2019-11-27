1c93cc97940bf7e0924f6c251197cc9f7fdfd4a8
joeyqy wang <hypercycle@litatom.com>
2019-11-27 20:29:48 +0800

    test

--- litatom/service/match_service.py
+++ litatom/service/match_service.py
@@ -203,6 +203,7 @@ class MatchService(object):
         score = redis_client.zscore(cls.MATCH_KEY_BY_REGION_GENDER(gender), fake_id)
         time_now = int(time.time())
         print time_now, score, 'fake_id', fake_id, '!' * 100
+        print time_now - score
         wait = 7
         if time_now - score > wait:
             return True
