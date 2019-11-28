bee373fac002343033530a6303dcdb3107ef30e1
joeyqy wang <hypercycle@litatom.com>
2019-11-28 20:52:55 +0800

    test

--- litatom/service/retain_ana_service.py
+++ litatom/service/retain_ana_service.py
@@ -66,7 +66,8 @@ class RetainAnaService(object):
             loc = obj.lang
             user_id = obj.user_id
             cls.USER_LOC[user_id] = loc
-            cls.DAY2BEFORE_ACT[user_id] = ['%s' % (action.action) for action in UserAction.objects(user_id=user_id, create_time__gte=action_low_time, create_time__lte=action_high_time)]
+            day2_actions = UserAction.objects(user_id=user_id, create_time__gte=action_low_time, create_time__lte=action_high_time)
+            cls.DAY2BEFORE_ACT[user_id] = ['%s' % (action.action) for action in day2_actions] if day2_actions else ['None']
             yestoday_acts = UserAction.objects(user_id=user_id, create_time__gte=action_ylow_time, create_time__lte=action_yhigh_time)
             if yestoday_acts:
                 cls.YESTODAY_ACT[user_id] = ['%s' % (action.action) for action in yestoday_acts]
