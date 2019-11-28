4509f8917003e2951ecb1c0fd96f00f856944e45
joeyqy wang <hypercycle@litatom.com>
2019-11-28 20:55:23 +0800

    test

--- litatom/service/retain_ana_service.py
+++ litatom/service/retain_ana_service.py
@@ -124,6 +124,6 @@ class RetainAnaService(object):
         # print res
         for i in range(min(100, len(losing_users))):
             user_id = losing_users[i]
-            res.append([user_id] + cls.DAY2BEFORE_ACT.get(user_id, []))
+            lst = [user_id] + cls.DAY2BEFORE_ACT.get(user_id, [])[-250:]
         print res
         write_data_to_xls(dst_addr, tb_head, res)
