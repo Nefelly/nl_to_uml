dd45414975a564c970a2ffab42037ff13ecd5b38
joeyqy wang <hypercycle@litatom.com>
2019-11-28 20:36:44 +0800

    test

--- scripts/retain_stat.py
+++ scripts/retain_stat.py
@@ -15,7 +15,7 @@ def run():
     dst_addr = '/data/retain_ana/%s.xlsx' % (datetime.datetime.now() - datetime.timedelta(days=2)).strftime('%Y-%m-%d')
     # dst_addr = '/data/statres/2019-11-15.xlsx'
     ensure_path(dst_addr)
-    RetainAnaService.out_port_result(dst_addr)
+    RetainAnaService.get_res(dst_addr)
     AlertService.send_file(["litatomwang@gmail.com", "w326571@126.com", '382365209@qq.com'], dst_addr)
 
 
