7fea98ae76bcd8a27b323ed2236223aa98ca3682
joeyqy wang <hypercycle@litatom.com>
2019-12-19 17:03:32 +0800

    test

--- litatom/service/sms_code_service.py
+++ litatom/service/sms_code_service.py
@@ -86,8 +86,8 @@ class SmsCodeService(object):
         zone_phone = validate_phone_number(zone_phone, zone)
         if not zone_phone:
             return cls.ERR_WORONG_TELEPHONE, False
-        # if zone.replace('+', '') != '86' and not User.get_by_phone(zone_phone):
-        #     return GlobalizationService.get_region_word('sms_could_not_register'), False
+        if zone.replace('+', '') != '86' and not User.get_by_phone(zone_phone):
+            return GlobalizationService.get_region_word('sms_could_not_register'), False
         if not TokenBucketService.get_token('send_lock' + zone_phone, 1, 1, 1, TEN_MINS, TEN_MINS):
             return '', True
         if zone.replace('+', '') == '86':
