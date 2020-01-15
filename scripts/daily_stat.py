5392f34f2bddcf3ef34254844b91d53ff20b76c2
CaucherWang <644513759@qq.com>
2020-01-15 11:25:23 +0800

    debug 12.0

--- scripts/daily_stat.py
+++ scripts/daily_stat.py
@@ -28,9 +28,9 @@ def run(stat_date=None):
         JournalService.out_port_result(dst_addr)
     #AlertService.send_file(["litatomwang@gmail.com", "w326571@126.com", '382365209@qq.com'], dst_addr)
     AlertService.send_file(["644513759@qq.com"], dst_addr)
-    # JournalService.ad_res(ad_addr)
+    JournalService.ad_res(ad_addr)
     #AlertService.send_file(["litatomwang@gmail.com", "w326571@126.com", '382365209@qq.com'], ad_addr)
-    # AlertService.send_file(["644513759@qq.com"], ad_addr)
+    AlertService.send_file(["644513759@qq.com"], ad_addr)
 
 
 if __name__ == "__main__":
