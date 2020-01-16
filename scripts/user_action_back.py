15ff6d860b557cb61c8f8eaeeb84253a32b98365
joeyqy wang <hypercycle@litatom.com>
2020-01-16 11:17:29 +0800

    test

--- scripts/user_action_back.py
+++ scripts/user_action_back.py
@@ -41,6 +41,6 @@ def clean_old_records():
 if __name__ == "__main__":
     print "started at", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
     back()
-    time.sleep(10)
+    time.sleep(500)
     clean_old_records()
     print "ended at", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
