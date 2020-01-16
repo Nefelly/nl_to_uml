4be48b59e8d857433b4a8afede938f5e4830703e
joeyqy wang <hypercycle@litatom.com>
2020-01-16 22:36:19 +0800

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ exec 2>&1
 #source /data/lit/bin/activate&&pip install langid
 #source /data/lit/bin/activate&&pip install pillow
 source /data/lit/bin/activate&&pip install tls-sig-api-v2
+source /data/lit/bin/activate&&pip install -U aliyun-log-python-sdk
 #cd /data/litatom
 
 python <<EOF
