155f6a528cad9989d744935f5486deaeeae560d1
joeyqy wang <hypercycle@litatom.com>
2020-02-05 22:15:07 +0800

    test

--- scripts/mq_user_action_batch.py
+++ scripts/mq_user_action_batch.py
@@ -68,16 +68,16 @@ def feed_consum():
                 ).start()
 
 def run():
-    mutex_f = '/var/run/%s.mutex' % __file__.split('/')[-1].replace('.py', '')
-    if setting.IS_DEV:
-        mutex_f += 'dev'
-    f = open(mutex_f, 'w')
-    try:
-        fcntl.flock(f,
-                    fcntl.LOCK_EX|fcntl.LOCK_NB)
-    except:
-        print 'program already in run'
-        sys.exit(0)
+    # mutex_f = '/var/run/%s.mutex' % __file__.split('/')[-1].replace('.py', '')
+    # if setting.IS_DEV:
+    #     mutex_f += 'dev'
+    # f = open(mutex_f, 'w')
+    # try:
+    #     fcntl.flock(f,
+    #                 fcntl.LOCK_EX|fcntl.LOCK_NB)
+    # except:
+    #     print 'program already in run'
+    #     sys.exit(0)
     thread_num = 10
     threads = []
     for i in range(thread_num):
