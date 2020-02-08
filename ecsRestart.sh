f8b4eec4e8e14bd79070d1155681977f71a231d5
joeyqy wang <hypercycle@litatom.com>
2020-02-08 14:20:54 +0800

    test

--- ecsRestart.sh
+++ ecsRestart.sh
@@ -4,6 +4,7 @@ set -e
 exec 2>&1
 
 /opt/mongodb/bin/mongod --config /opt/mongodb/bin/mongodb.conf 2>&1 &
+/opt/mongodb/bin/mongod --config /opt/mongodb/bin/relationmongodbt.conf 2>&1 &
 sudo service nginx restart
 runsvdir -P /etc/service 2>&1 &
 service rabbitmq-server restart 2>&1 &
