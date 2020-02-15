8441906a65425e4cfe5b74eba5caf6ca56da5b98
CaucherWang <644513759@qq.com>
2020-02-15 19:27:10 +0800

    test attribute

--- scripts/ali_log_test.py
+++ scripts/ali_log_test.py
@@ -3,7 +3,7 @@ from time import time
 
 
 def run():
-    ali_log_service.AliLogService.get_log_by_time(project='litatom', logstore='litatomstore',
+    ali_log_service.AliLogService.get_log_by_time(project='litatom', logstore='litatomstore',size=100,
                                                   attributes=['request_uri', 'time', 'upstream_response_time'])
 
 
