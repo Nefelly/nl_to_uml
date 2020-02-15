48e94b43d5595d448a0a9348bf2e15450052cfdb
CaucherWang <644513759@qq.com>
2020-02-15 15:58:33 +0800

    get_histogram

--- scripts/ali_log_test.py
+++ scripts/ali_log_test.py
@@ -3,10 +3,7 @@ from time import time
 
 
 def run():
-    print("debug1")
-    ali_log_service.AliLogService.get_log_by_time_and_topic(from_time=int(time()-500),to_time=int(time()))
-    print("debug2")
-    ali_log_service.AliLogService.get_log_by_time(from_time=int(time()-500),to_time=int(time()))
+    ali_log_service.AliLogService.get_histogram_by_time_and_topic()
 
 
 if __name__ == "__main__":
