beaf4f895d737b93e8f333ce77de313f33f4d8c5
CaucherWang <644513759@qq.com>
2020-02-16 21:33:07 +0800

    query

--- scripts/ali_log_test.py
+++ scripts/ali_log_test.py
@@ -15,45 +15,53 @@ def run():
         start = start_time[0:11] + str_hour + start_time[13:]
         end = end_time[0:11] + str_hour + end_time[13:]
         start_match_logs = AliLogService.get_log_by_time(
-            query='remark:startMatch and action:match', size=2000, from_time=start,
+            query='remark:startMatch and action:match|select session_id,user_id,', size=20, from_time=start,
             to_time=end)
+        for start_match_log in start_match_logs.logs:
+            contents = start_match_log.get_contents()
+            user_id = contents['user_id']
+            session_id = contents['session_id']
+            start_time = start_match_log.get_time()
+            match_success_text_logs = AliLogService.get_log_by_time(from_time=start_time, to_time=start_time + 180,
+                                                                    query='remark:matchSuccesstext and '
+                                                                          'action:match and user_id:' + user_id +
+                                                                          ' and session_id:' + session_id).logs
+            match_success_voice_logs = AliLogService.get_log_by_time(from_time=start_time, to_time=start_time + 180,
+                                                                     query='remark:matchSuccessvoice and '
+                                                                           'action:match and user_id:' + user_id +
+                                                                           ' and session_id:' + session_id).logs
+            match_success_video_logs = AliLogService.get_log_by_time(from_time=start_time, to_time=start_time + 180,
+                                                                     query='remark:matchSuccessvideo and '
+                                                                           'action:match and user_id:' + user_id +
+                                                                           ' and session_id:' + session_id).logs
+            match_success_logs = []
+            if match_success_voice_logs:
+                match_success_logs = match_success_voice_logs
+                contents['matchType'] = 'voice'
+            elif match_success_video_logs:
+                match_success_logs = match_success_video_logs
+                contents['matchType'] = 'video'
+            elif match_success_text_logs:
+                match_success_logs = match_success_text_logs
+                contents['matchType'] = 'text'
+            if match_success_logs:
+                min_time = match_success_logs[0].get_time()
+                if len(match_success_logs) > 1:
+                    for log in match_success_logs:
+                        tmp_time = log.get_time()
+                        if tmp_time < min_time:
+                            min_time = tmp_time
+                contents['matchTime'] = min_time - start_time
+            else:
+                continue
         start_match_logs.log_print()
+
     # for log_set in start_match_logs:
     #     for start_match_log in log_set.logs:
     #         contents = start_match_log.get_contents()
     #         user_id = contents['user_id']
     #         session_id = contents['session_id']
     #         time = start_match_log.get_time()
-    #         match_success_text_logs = AliLogService.get_log_by_time_and_topic(from_time=time, to_time=time + 180,
-    #                                                                           query='remark:matchSuccesstext and '
-    #                                                                                 'action:match and user_id:' + user_id +
-    #                                                                                 ' and session_id:' + session_id).logs
-    #         match_success_voice_logs = AliLogService.get_log_by_time_and_topic(from_time=time, to_time=time + 180,
-    #                                                                            query='remark:matchSuccessvoice and '
-    #                                                                                  'action:match and user_id:' + user_id +
-    #                                                                                  ' and session_id:' + session_id).logs
-    #         match_success_video_logs = AliLogService.get_log_by_time_and_topic(from_time=time, to_time=time + 180,
-    #                                                                            query='remark:matchSuccessvideo and '
-    #                                                                                  'action:match and user_id:' + user_id +
-    #                                                                                  ' and session_id:' + session_id).logs
-    #         match_success_logs = []
-    #         if match_success_voice_logs:
-    #             match_success_logs = match_success_voice_logs
-    #             contents['matchType'] = 'voice'
-    #         elif match_success_video_logs:
-    #             match_success_logs = match_success_video_logs
-    #             contents['matchType'] = 'video'
-    #         elif match_success_text_logs:
-    #             match_success_logs = match_success_text_logs
-    #             contents['matchType'] = 'text'
-    #         if match_success_logs:
-    #             min_time = match_success_logs[0].get_time()
-    #             if len(match_success_logs) > 1:
-    #                 for log in match_success_logs:
-    #                     tmp_time = log.get_time()
-    #                     if tmp_time < min_time:
-    #                         min_time = tmp_time
-    #             contents['matchTime'] = min_time - time
     #             leave_logs = []
     #             if contents['matchType'] == 'voice' or contents['matchType'] == 'video':
     #                 leave_logs = AliLogService.get_log_by_time_and_topic(from_time=min_time, to_time=min_time + 420,
