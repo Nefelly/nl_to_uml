b6714605e5cbf5dd1efabccd86ea04ade67aa426
CaucherWang <644513759@qq.com>
2020-02-16 13:58:18 +0800

    query

--- scripts/ali_log_test.py
+++ scripts/ali_log_test.py
@@ -3,14 +3,49 @@ from time import time
 
 
 def run():
-    start_match_logs = AliLogService.get_log_by_time(query='remark:startMatch and action:match',size=100,
+    start_match_logs = AliLogService.get_log_by_time(query='remark:startMatch and action:match', size=100,
                                                      from_time="2020-02-15 00:00:00+8:00",
                                                      to_time="2020-02-15 00:22:00+8:00")
+    for start_match_log in start_match_logs.logs:
+        contents = start_match_log.get_contents()
+        user_id = contents['user_id']
+        session_id = contents['session_id']
+        time = start_match_log.get_time()
+        match_success_text_logs = AliLogService.get_log_by_time_and_topic(from_time=time, to_time=time + 180,
+                                                                          query='remark:matchSuccesstext and '
+                                                                                'action:match and user_id:' + user_id +
+                                                                                'and session_id:' + session_id).logs
+        match_success_voice_logs = AliLogService.get_log_by_time_and_topic(from_time=time, to_time=time + 180,
+                                                                           query='remark:matchSuccessvoice and '
+                                                                                 'action:match and user_id:' + user_id +
+                                                                                 'and session_id:' + session_id).logs
+        match_success_video_logs = AliLogService.get_log_by_time_and_topic(from_time=time, to_time=time + 180,
+                                                                           query='remark:matchSuccessvideo and '
+                                                                                 'action:match and user_id:' + user_id +
+                                                                                 'and session_id:' + session_id).logs
+        if not match_success_text_logs and not match_success_video_logs and not match_success_voice_logs:
+            continue
+        match_success_logs = []
+        if match_success_voice_logs:
+            match_success_logs = match_success_voice_logs
+            contents['matchType'] = 'voice'
+        elif match_success_video_logs:
+            match_success_logs = match_success_video_logs
+            contents['matchType'] = 'video'
+        elif match_success_text_logs:
+            match_success_logs = match_success_text_logs
+            contents['matchType'] = 'text'
+        if match_success_logs:
+            min_time = match_success_logs[0].get_time()
+            if len(match_success_logs) > 1:
+                for log in match_success_logs:
+                    tmp_time = log.get_time()
+                    if tmp_time < min_time:
+                        min_time = tmp_time
+            contents['matchTime'] = min_time
+        else:
+            contents['matchTime'] = 180
     start_match_logs.log_print()
-    # for start_match_log in start_match_logs.logs:
-    #     contents = start_match_log.get_contents()
-    #     time = start_match_log.get_time()
-    #     match_success_text_logs = AliLogService.get_log_by_time_and_topic(from_time=time, to_time=time + 180, query='remark:matchSuccesstext and action:match')
 
 
 if __name__ == "__main__":
