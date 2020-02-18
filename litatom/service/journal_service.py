13b39282e48d2b10e4d4f3a8c3c771d80b64e0f5
CaucherWang <644513759@qq.com>
2020-02-18 15:23:41 +0800

    test

--- litatom/service/journal_service.py
+++ litatom/service/journal_service.py
@@ -30,19 +30,20 @@ logger = logging.getLogger(__name__)
 
 redis_client = RedisClient()['lit']
 
+
 class JournalService(object):
     IS_TESTING = False
-    DATE_DIS = datetime.timedelta(hours=15)
+    DATE_DIS = datetime.timedelta(hours=0)
 
-    USER_LOC = {}   # user_id:language
-    NEW_USER_LOC = {}   # 最近一天创建的用户为新用户 user_id:language
-    LOC_STATED = ['TH', 'VN', 'PH'] # new_TH, count_TH
+    USER_LOC = {}  # user_id:language
+    NEW_USER_LOC = {}  # 最近一天创建的用户为新用户 user_id:language
+    LOC_STATED = ['TH', 'VN', 'PH']  # new_TH, count_TH
     CACHED_RES = {}
     ZERO_TODAY = None
-    GENDERS = ['boy','girl']
-    USER_GEN = {}   # user_id:gender
+    GENDERS = ['boy', 'girl']
+    USER_GEN = {}  # user_id:gender
     ALI_LOG_STORE = ['UserAction']  # 存在ali_log_service的部分表
-    CACHED_ALI_LOG = None   # 迭代器类型
+    CACHED_ALI_LOG = None  # 迭代器类型
 
     @classmethod
     def load_user_loc(cls):
@@ -80,7 +81,6 @@ class JournalService(object):
         from_time, to_time = cls._get_alilog_time_str(date)
         cls.CACHED_ALI_LOG = AliLogService.get_all_log_by_time_and_topic(from_time=from_time, to_time=to_time)
 
-
     @classmethod
     def get_journal_items(cls, stat_type):
         res = []
@@ -109,15 +109,16 @@ class JournalService(object):
             "id": str(item.id),
             "name": item.name
         }
-        from_time, to_time= cls._get_alilog_time_str(date)
+        from_time, to_time = cls._get_alilog_time_str(date)
         loc_cnts = {}
         for loc in cls.LOC_STATED:
             loc_cnts[loc] = 0.0
         gender_cnts = {}
         for gender in cls.GENDERS:
             gender_cnts[gender] = 0.0
-        resp_set = AliLogService.get_log_by_time_and_topic(from_time=from_time, to_time=to_time, query='*|select distinct user_id,location',size=-1)
-        cnt =0.0
+        resp_set = AliLogService.get_log_by_time_and_topic(from_time=from_time, to_time=to_time,
+                                                           query='*|select distinct user_id,location', size=-1)
+        cnt = 0.0
         for resp in resp_set:
             cnt += resp.get_count()
             for log in resp.logs:
@@ -173,6 +174,7 @@ class JournalService(object):
         :param need_loc: 如果need_loc=True，会针对每个location，分别基于其它统计量的'loc'结果，进行计算，key为各种'loc'
         :return: 一个dict，key为'num'的为主要计算结果，基于其它统计量的'num'结果
         """
+
         def cal_exp(exp):
             try:
                 if '/' in exp:
@@ -180,6 +182,7 @@ class JournalService(object):
                 return eval(exp)
             except:
                 return 0
+
         # m是一个字典，其中包含Expression各个操作数，每个操作数实际上是一个统计量
         m = cls.get_objids(expression)
         # 在res_m中，对各个统计量递归的分别计算结果
@@ -197,7 +200,7 @@ class JournalService(object):
         for gender in cls.GENDERS:
             tmp_exp = expression
             for el in res_m:
-                tmp_exp=tmp_exp.replace(el,str(res_m[el][gender]))
+                tmp_exp = tmp_exp.replace(el, str(res_m[el][gender]))
             loc_cnts[gender] = cal_exp(tmp_exp)
         # 如果需要location信息，则对每个location的数量分别计算
         if need_loc:
@@ -230,7 +233,7 @@ class JournalService(object):
             time_str = "%s__gte=%r, %s__lte=%r" % (judge_field, zeroYesterday, judge_field, zeroToday)
         else:
             time_str = "%s__gte=%r, %s__lte=%r" % (
-            judge_field, date_to_int_time(zeroYesterday), judge_field, date_to_int_time(zeroToday))
+                judge_field, date_to_int_time(zeroYesterday), judge_field, date_to_int_time(zeroToday))
         return time_str
 
     @classmethod
@@ -247,12 +250,12 @@ class JournalService(object):
         if cls.ZERO_TODAY:
             zeroToday = cls.ZERO_TODAY
         zeroYesterday = next_date(zeroToday, -1) + cls.DATE_DIS
-        from_time =zeroYesterday.strftime("%Y-%m-%d %H:%M:%S+8:00")
+        from_time = zeroYesterday.strftime("%Y-%m-%d %H:%M:%S+8:00")
         to_time = zeroToday.strftime("%Y-%m-%d %H:%M:%S+8:00")
-        return from_time,to_time
+        return from_time, to_time
 
     @classmethod
-    def _get_stat_date(cls,date):
+    def _get_stat_date(cls, date):
         """
         :param date: datetime类型
         :return: datetime类型，为输入日期的前一天
@@ -270,12 +273,14 @@ class JournalService(object):
         :param need_loc:
         :return: 返回一个dict，有id,name,num,loc,new_loc,count_loc各个字段
         """
+
         def check_valid_string(word):
             chars = string.ascii_letters + '_' + string.digits
             for chr in word:
                 if chr not in chars:
                     return False
             return True
+
         if cls.CACHED_RES.get(item_id):
             return cls.CACHED_RES[item_id]
 
@@ -330,13 +335,14 @@ class JournalService(object):
             if not expression:
                 resp_set = cls.CACHED_ALI_LOG
             else:
-                expression = expression.replace('=',':')
-                expression = expression.replace(','," and ")
-                resp_set = AliLogService.get_all_log_by_time_and_topic(from_time=from_time, to_time=to_time, query=expression)
+                expression = expression.replace('=', ':')
+                expression = expression.replace(',', " and ")
+                resp_set = AliLogService.get_all_log_by_time_and_topic(from_time=from_time, to_time=to_time,
+                                                                       query=expression)
             cnt = 0
-            gender_cnts={}
+            gender_cnts = {}
             for gender in cls.GENDERS:
-                gender_cnts[gender]=0.0
+                gender_cnts[gender] = 0.0
             loc_cnts = {}
             if need_loc:
                 for loc in cls.LOC_STATED:
@@ -356,7 +362,7 @@ class JournalService(object):
                         for log in resp.logs:
                             contents = log.get_contents()
                             user_id = contents['user_id']
-                            gender=cls.USER_GEN.get(user_id)
+                            gender = cls.USER_GEN.get(user_id)
                             if gender in gender_cnts:
                                 gender_cnts[gender] += 1
                             loc = cls.USER_LOC.get(user_id)
@@ -373,7 +379,7 @@ class JournalService(object):
                     for log in resp.logs:
                         contents = log.get_contents()
                         user_id = contents['user_id']
-                        gender=cls.USER_GEN.get(user_id)
+                        gender = cls.USER_GEN.get(user_id)
                         if gender in gender_cnts:
                             gender_cnts[gender] += 1
             res = {
@@ -398,9 +404,9 @@ class JournalService(object):
             cnt = eval(exc_str)
             if not cnt:
                 cnt = 0
-            gender_cnts={}
+            gender_cnts = {}
             for gender in cls.GENDERS:
-                gender_cnts[gender]=0.0
+                gender_cnts[gender] = 0.0
             loc_cnts = {}
             table_user_id = {
                 "Report": "target_uid",
@@ -427,7 +433,7 @@ class JournalService(object):
                             # user_id = str(obj.target_uid)
                         else:
                             user_id = obj.user_id
-                        gender=cls.USER_GEN.get(str(user_id))
+                        gender = cls.USER_GEN.get(str(user_id))
                         if gender in gender_cnts:
                             gender_cnts[gender] += 1
                         loc = cls.USER_LOC.get(user_id)
@@ -448,7 +454,7 @@ class JournalService(object):
                         user_id = getattr(obj, table_user_id[table_name])
                     else:
                         user_id = obj.user_id
-                    gender=cls.USER_GEN.get(str(user_id))
+                    gender = cls.USER_GEN.get(str(user_id))
                     if gender in gender_cnts:
                         gender_cnts[gender] += 1
             res = {
@@ -483,27 +489,27 @@ class JournalService(object):
                 # m为根据该统计量的id计算得到的结果
                 m = cls.cal_by_id(str(item.id))
                 name, num = m['name'], m['num']
-                #ali_log.append(('name',name))
-                #ali_log.append(('num',str(num)))
+                # ali_log.append(('name',name))
+                # ali_log.append(('num',str(num)))
                 gender_cnt = [m[gender] for gender in cls.GENDERS]
                 # for gender in cls.GENDERS:
                 #    ali_log.append((gender,str(m[gender])))
                 region_cnt = [m[loc] for loc in cls.LOC_STATED]
-                #for loc in cls.LOC_STATED:
+                # for loc in cls.LOC_STATED:
                 #    ali_log.append((loc,str(m[loc])))
-                #ali_log.append(('total_avr',str(num/daily_m['num'])))
+                # ali_log.append(('total_avr',str(num/daily_m['num'])))
                 avr_cnt = []
                 for loc in cls.LOC_STATED:
                     if daily_m[loc]:
-                        data = round(m.get(loc, 0)/daily_m[loc], 4)
+                        data = round(m.get(loc, 0) / daily_m[loc], 4)
                         avr_cnt.append(data)
-                 #       ali_log.append((loc+'avr',str(data)))
+                    #       ali_log.append((loc+'avr',str(data)))
                     else:
                         avr_cnt.append(0)
-                 #       ali_log.append((loc+'avr','0'))
-                res_lst.append([name, num] + gender_cnt + region_cnt + [num/daily_m['num']] + avr_cnt)
-                #AliLogService.put_daily_stat(ali_log,topic='business_type')
-                print(name,num)
+                #       ali_log.append((loc+'avr','0'))
+                res_lst.append([name, num] + gender_cnt + region_cnt + [num / daily_m['num']] + avr_cnt)
+                # AliLogService.put_daily_stat(ali_log,topic='business_type')
+                print(name, num)
                 cnt += 1
             except Exception as e:
                 print(e)
@@ -513,7 +519,9 @@ class JournalService(object):
         # dst_addr = '/data/statres/%s.xlsx' % now_date_key()
         # ensure_path(dst_addr)
         # print res_lst
-        write_data_to_xls(dst_addr, [u'名字', u'数量'] + cls.GENDERS + cls.LOC_STATED + ['total avr'] + [el + 'avr' for el in cls.LOC_STATED], res_lst)
+        write_data_to_xls(dst_addr,
+                          [u'名字', u'数量'] + cls.GENDERS + cls.LOC_STATED + ['total avr'] + [el + 'avr' for el in
+                                                                                           cls.LOC_STATED], res_lst)
 
     @classmethod
     def ad_res(cls, dst_addr, date):
@@ -522,7 +530,7 @@ class JournalService(object):
         cnt = 0
         daily_m = cls.daily_active(StatItems.objects(name=u'抽样日活').first())
         cls.DATE_DIS = datetime.timedelta(hours=-16)
-        stat_date=cls._get_stat_date(date)
+        stat_date = cls._get_stat_date(date)
         for item in StatItems.get_items_by_type(StatItems.AD_TYPE):
             try:
                 m = cls.cal_by_id(str(item.id))
@@ -537,9 +545,9 @@ class JournalService(object):
                 avr_cnt = []
                 for loc in cls.LOC_STATED:
                     if daily_m[loc]:
-                        data=round(m.get(loc, 0) / daily_m[loc], 4)
+                        data = round(m.get(loc, 0) / daily_m[loc], 4)
                         avr_cnt.append(data)
-                #        ali_log.append((loc+'avr',str(data)))
+                    #        ali_log.append((loc+'avr',str(data)))
                     else:
                         avr_cnt.append(0)
                 #        ali_log.append((loc+'avr','0'))
