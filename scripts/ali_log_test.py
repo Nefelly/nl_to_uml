fc06bf9c5e4a1629d1479fc0e50658a41683731b
CaucherWang <644513759@qq.com>
2020-02-19 22:34:40 +0800

    test select regex

--- scripts/ali_log_test.py
+++ scripts/ali_log_test.py
@@ -4,7 +4,7 @@ from litatom.service import AliLogService
 def run():
     res_set = AliLogService.get_log_by_time_and_topic(project='litatom', query="request_uri:/api/sns/v1/lit/user AND "
                                                                                "request_method:GET|SELECT regexp_extract(request_uri, "
-                                                                               "'\^\[0-9a-f\]\{24\}') as request_uri,"
+                                                                               "'\^\[0-9a-f\]\{24\}'),request_uri,"
                                                                                "upstream_response_time",
                                                       size=100, logstore='litatomstore')
     for res in res_set:
