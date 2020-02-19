9688353983da2a21f1f8072753acb065376a6afc
CaucherWang <644513759@qq.com>
2020-02-19 22:28:47 +0800

    test select regex

--- scripts/ali_log_test.py
+++ scripts/ali_log_test.py
@@ -2,8 +2,8 @@ from litatom.service import AliLogService
 
 
 def run():
-    res_set = AliLogService.get_log_by_time_and_topic(query="request_uri:/api/sns/v1/lit/user AND "
-                                                            "request_method:GET|SELECT regexp_extract(request_uri, "
+    res_set = AliLogService.get_log_by_time_and_topic(query="request_uri\:/api/sns/v1/lit/user AND "
+                                                            "request_method\:GET|SELECT regexp_extract(request_uri, "
                                                             "'\^\[0-9a-f\]\{24\}'),upstream_response_time", size=100)
     for res in res_set:
         res.log_print()
