b9247ed0a3c4c5219950547d4137e87034855c32
joeyqy wang <hypercycle@litatom.com>
2020-02-23 23:06:43 +0800

    test

--- scripts/api_monitor.py
+++ scripts/api_monitor.py
@@ -1,7 +1,20 @@
+import sys
+import fcntl
 from litatom.service import MonitorService
 
+from hendrix.conf import setting
 
 def run():
+    mutex_f = '/var/run/%s.mutex' % __file__.split('/')[-1].replace('.py', '')
+    if setting.IS_DEV:
+        mutex_f += 'dev'
+    f = open(mutex_f, 'w')
+    try:
+        fcntl.flock(f,
+                    fcntl.LOCK_EX | fcntl.LOCK_NB)
+    except:
+        print 'program already in run'
+        sys.exit(0)
     MonitorService.monitor_report()
 
 
