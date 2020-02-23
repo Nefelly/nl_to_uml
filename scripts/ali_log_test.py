d0f3262d99e0ee3d538963a2d3fef80949ac11c8
CaucherWang <644513759@qq.com>
2020-02-23 20:42:04 +0800

    test query

--- scripts/ali_log_test.py
+++ scripts/ali_log_test.py
@@ -2,7 +2,7 @@ from litatom.service import AliLogService
 
 
 def run():
-    res_set = AliLogService.get_log_atom(project='litatom', size=-1, logstore='litatomstore',
+    res_set = AliLogService.get_log_atom(project='litatom', logstore='litatomstore',
                                          query='request_uri:/api/sns/v1/lit/user/info AND request_method:POST'
                                                 '|SELECT avg(upstream_response_time) as avg_resp_time,'
                                                 'count(1) as called_num,avg(status) as avg_status',
