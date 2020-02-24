961d3c5fef034de834fc552c3d88305acc08be91
CaucherWang <644513759@qq.com>
2020-02-24 15:08:41 +0800

    test sql

--- scripts/ali_log_test.py
+++ scripts/ali_log_test.py
@@ -3,7 +3,7 @@ from litatom.service import AliLogService
 
 def run():
     res_set = AliLogService.get_log_atom(project='litatomaction', logstore='litatomactionstore',
-                                         query='action:match and remark:matchSuccess*|SELECT COUNT(1) as res GROUP BY user_id HAVING res=1',
+                                         query='action:match and remark:matchSuccess*|SELECT COUNT(1) as res GROUP BY user_id HAVING res=1 limit 1000000',
                                          from_time='2020-02-22 20:30:00+8:00',
                                          to_time='2020-02-23 20:30:00+8:00')
     for log in res_set.logs:
