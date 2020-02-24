791fa2e1a84cfa2290859b3b62e54d3d5c9a32c0
CaucherWang <644513759@qq.com>
2020-02-24 19:20:13 +0800

    debug

--- scripts/ali_log_test.py
+++ scripts/ali_log_test.py
@@ -2,7 +2,7 @@ from litatom.service import AliLogService
 
 
 def run():
-    resp = AliLogService.get_log_by_time_and_topic(from_time='2020-02-23 19:12:00+8:00',to_time='2020-02-24 19:12:00+8:00',
+    resp = AliLogService.get_log_by_time_and_topic(from_time='2020-02-23 19:12:00+8:00',to_time='2020-02-24 19:12:00+8:00',size=100000,
                               query='name:deposit and diamonds:100|SELECT COUNT(DISTINCT user_id) as res',project='litatom-account',
                                                    logstore='account_flow')
 
