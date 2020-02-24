f323291bcc2c7353e011694f4604d33f7ee81cb8
CaucherWang <644513759@qq.com>
2020-02-24 19:17:58 +0800

    debug

--- scripts/ali_log_test.py
+++ scripts/ali_log_test.py
@@ -3,7 +3,7 @@ from litatom.service import DiamStatService
 
 def run():
     resp = DiamStatService.fetch_log(from_time='2020-02-23 19:12:00+8:00',to_time='2020-02-24 19:12:00+8:00',
-                              query='name:deposit and diamonds:100|SELECT COUNT(DISTINCT user_id) as res)')
+                              query='name:deposit and diamonds:100|SELECT COUNT(DISTINCT user_id) as res')
 
     resp.log_print()
 
