3426c8722b3bed439e6408d455714ca7abf49f2b
CaucherWang <644513759@qq.com>
2020-02-25 10:23:54 +0800

    debug

--- scripts/account_stat.py
+++ scripts/account_stat.py
@@ -13,6 +13,7 @@ def run(stat_date=datetime.datetime.now()):
             '%Y-%m-%d')
         match_addr = '/data/match_account_stat/%s/xlsx' % (stat_date - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
     ensure_path(dst_addr)
+    ensure_path(match_addr)
     DiamStatService.diam_stat_report(dst_addr, stat_date)
     AlertService.send_file(['644513759@qq.com'], dst_addr)
     DiamStatService.diam_free_report(match_addr,stat_date)
