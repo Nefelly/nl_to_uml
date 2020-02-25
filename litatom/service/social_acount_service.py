17c84445dcef92f62005b4c6f7ebfad1fddbf693
joeyqy wang <hypercycle@litatom.com>
2020-02-25 22:28:07 +0800

    test

--- litatom/service/social_acount_service.py
+++ litatom/service/social_acount_service.py
@@ -75,8 +75,8 @@ class GoogleService(object):
         datas = {
             "grant_type": "authorization_code",
             "code": "4/wwHGVxUL6wmwx75MC1bvRr1i_S0jQuj3ldhKxbO-CL1mzyPgECZS332EK9KfrEXzeO1kKIullQZC3K95Lpk4xhY" if not code else code,
-            "client_id": "272687572250-q38h6dlnru40d39fa0cbais936r93064.apps.googleusercontent.com",
-            "client_secret": "",
+            "client_id": cls.CLIENT_ID,
+            "client_secret": cls.CLIENT_SECRET,
             "redirect_uri": redirect_uri
         }
         params = urllib.urlencode(datas)
