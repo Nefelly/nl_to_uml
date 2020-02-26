6fa6ac1b7a1ae889fb427b5bf9abf56594319daa
CaucherWang <644513759@qq.com>
2020-02-26 16:04:12 +0800

    test resp

--- litatom/service/social_acount_service.py
+++ litatom/service/social_acount_service.py
@@ -130,6 +130,7 @@ class GoogleService(object):
         # resp = req.get(real_url)
         # print(resp)
         print('#',access_token)
+        print('##', cls.ACCESS_TOKEN)
         url = 'https://www.googleapis.com/androidpublisher/v3/applications/packageName/purchases/products/productId/tokens/purchaseToken'
         data = {'access_token':access_token}
         real_url = cls.url_append_param(url,data)
