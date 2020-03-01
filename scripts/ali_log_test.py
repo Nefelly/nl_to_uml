784ab5b2e57a303639680385c0731383407055e0
CaucherWang <644513759@qq.com>
2020-03-01 20:28:15 +0800

    debug

---
+++ scripts/ali_log_test.py
@@ -0,0 +1,15 @@
+from litatom.service import AliLogService
+
+def run():
+    resp_set = AliLogService.get_log_by_time_and_topic(from_time='2020-03-01 00:00:00', to_time='2020-03-01 12:00:00',
+                                                       query='*|select distinct user_id,location', size=-1)
+    num = 0
+    for resp in resp_set:
+        num+=1
+        print(num)
+        print(resp.get_count())
+        print(resp.logs.count())
+
+
+if __name__ == '__main__':
+    run()
