b9edd4cc5ea89d34b115d49a35a8763bd23dd469
CaucherWang <644513759@qq.com>
2020-03-01 20:29:39 +0800

    debug

--- scripts/ali_log_test.py
+++ scripts/ali_log_test.py
@@ -1,7 +1,7 @@
 from litatom.service import AliLogService
 
 def run():
-    resp_set = AliLogService.get_log_by_time_and_topic(from_time='2020-03-01 00:00:00', to_time='2020-03-01 12:00:00',
+    resp_set = AliLogService.get_log_by_time_and_topic(from_time='2020-03-01 00:00:00+8:00', to_time='2020-03-01 12:00:00+8:00',
                                                        query='*|select distinct user_id,location', size=-1)
     num = 0
     for resp in resp_set:
