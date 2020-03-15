165beed79830f226bd1008e7d6e5219bbf1f147b
CaucherWang <644513759@qq.com>
2020-03-15 15:15:32 +0800

    test feature

--- scripts/account_stat.py
+++ scripts/account_stat.py
@@ -15,11 +15,9 @@ def run(stat_date=get_zero_today()):
     ensure_path(dst_addr)
     ensure_path(match_addr)
     DiamStatService.diam_stat_report_7_days(dst_addr, stat_date)
-    AlertService.send_file(['644513759@qq.com'], dst_addr)
-
-    # AlertService.send_file(["litatomwang@gmail.com", "w326571@126.com", '382365209@qq.com','644513759@qq.com'], dst_addr)
-    # DiamStatService.diam_free_report(match_addr,stat_date)
-    # AlertService.send_file(["litatomwang@gmail.com", "w326571@126.com", '382365209@qq.com','644513759@qq.com'], match_addr)
+    AlertService.send_file(["litatomwang@gmail.com", "w326571@126.com", '382365209@qq.com','644513759@qq.com'], dst_addr)
+    DiamStatService.diam_free_report(match_addr,stat_date)
+    AlertService.send_file(["litatomwang@gmail.com", "w326571@126.com", '382365209@qq.com','644513759@qq.com'], match_addr)
 
 
 if __name__ == "__main__":
