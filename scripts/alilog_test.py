36fc9f1d8aa97279278993c7d26944d6f2b1c04e
CaucherWang <644513759@qq.com>
2020-03-18 17:46:25 +0800

    debug

--- scripts/alilog_test.py
+++ scripts/alilog_test.py
@@ -7,7 +7,9 @@ from litatom.util import *
 def run():
     to_time = get_zero_today()
     from_time = next_date(get_zero_today(),-1)
+    print(from_time,to_time)
     users = User.get_by_create_time(from_time,to_time)
+    print(users)
     for user in users:
         print(str(user.id))
 
