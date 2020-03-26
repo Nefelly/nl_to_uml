d3d5db6b1ed89ce3aa50d7ca94f6de4ed3781c0f
joeyqy wang <hypercycle@litatom.com>
2020-03-26 20:04:46 +0800

    test

--- litatom/request.py
+++ litatom/request.py
@@ -63,7 +63,7 @@ class LitatomRequest(flask.Request):
     @cached_property
     def platform(self):
         p = self.values.get('platform', '').lower()
-        print p, self.args
+        print p, self.args, '!!!!!'
         if p in [const.PLATFORM_ANDROID, const.PLATFORM_IOS]:
             return p
 
