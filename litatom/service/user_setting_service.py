73d90242cf2f92deb3e1acf3035e49dfb26d5403
joeyqy wang <hypercycle@litatom.com>
2020-03-26 20:06:01 +0800

    test

--- litatom/service/user_setting_service.py
+++ litatom/service/user_setting_service.py
@@ -69,7 +69,7 @@ class UserSettingService(object):
 
     @classmethod
     def get_setting_key(cls):
-        print request.is_ios, request.platform
+        print request.is_ios, request.platform, request.values
         return REDIS_SETTINGS_IOS if request.platform == PLATFORM_IOS else REDIS_SETTINGS_KEYS
 
     @classmethod
