3f0ecb40647545037e3c78e6edfb43b9123130ec
CaucherWang <644513759@qq.com>
2020-03-29 20:33:31 +0800

    redis-test

--- scripts/temp.py
+++ scripts/temp.py
@@ -1,10 +1,12 @@
-from litatom.service import AliLogService
+from litatom.redis import RedisClient
+from litatom.service import ShareStatService
+
+redis_client=RedisClient()['lit']
 
 
 def run():
-    AliLogService.put_logs([('user_id', '5e18529f3fff2253c8410e9e'), ('action', 'create_new_user')],
-                           logstore='shareaction')
-    AliLogService.put_logs([('clicker', '10.244.141.151'), ('action', 'click_share')], logstore='shareaction')
+    key=ShareStatService.get_clicker_key('101.94.131.160')
+    redis_client.set(key,1)
 
 
 if __name__ == '__main__':
