f644d7e279cf8abdeebe0477861521e4268c8b09
CaucherWang <644513759@qq.com>
2020-03-29 18:59:45 +0800

    test

---
+++ scripts/temp.py
@@ -0,0 +1,8 @@
+from litatom.service import AliLogService
+
+def run():
+    AliLogService.put_logs([('user_id','5e18529f3fff2253c8410e9e'),('action','create_new_user')],logstore='shareaction')
+    AliLogService.put_logs([('clicker','10.244.141.151'),('action','click_share')],logstore='shareaction')
+
+if __name__=='__main__':
+    run()
