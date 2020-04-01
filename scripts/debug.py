d8ab266ca8f9d49ded1dc13f576b312e0bbb4553
CaucherWang <644513759@qq.com>
2020-04-01 13:15:06 +0800

    debug

--- scripts/debug.py
+++ scripts/debug.py
@@ -5,7 +5,9 @@ def run():
     resp_set=AliLogService.get_log_atom(from_time='2020-03-31 00:00:00+8:00',
                                         to_time='2020-04-01 00:00:00+8:00',
                                         project='litatom-account',logstore='account_flow',
-                                        query='name:unban_by_diamonds | SELECT sum(diamonds) as res')
+                                        # query='name:unban_by_diamonds | SELECT sum(diamonds) as res'
+                                        query='name:unban_by_diamonds | SELECT COUNT(1) as res'
+                                        )
     resp_set.log_print()
 
 if __name__=='__main__':
