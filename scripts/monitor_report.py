a49f56776de785d999f62789ab4f6037dc9b3e45
CaucherWang <644513759@qq.com>
2020-04-02 15:12:40 +0800

    debvy

--- scripts/monitor_report.py
+++ scripts/monitor_report.py
@@ -10,7 +10,7 @@ from litatom.util import (
 def run(start_time=datetime.datetime.now() + datetime.timedelta(minutes=-5),
         end_time=datetime.datetime.now()):
     dst_addr = "api_monitor:%s"% (format_standard_time(start_time)+'-'+format_standard_time(end_time))
-    MonitorService.monitor_report()
+    MonitorService.monitor_report(dst_addr, start_time, end_time)
 
 
 if __name__ == '__main__':
