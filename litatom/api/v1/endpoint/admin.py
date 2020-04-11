7a1c2752cfaca256a58637e389ec9af4592027af
joeyqy wang <hypercycle@litatom.com>
2020-04-11 19:33:08 +0800

    test

--- litatom/api/v1/endpoint/admin.py
+++ litatom/api/v1/endpoint/admin.py
@@ -60,10 +60,10 @@ from  ....const import (
 )
 logger = logging.getLogger(__name__)
 app = Flask(__name__, static_folder='static')
-Compress(app)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = timedelta(seconds=1)
 app.config['COMPRESS_MIN_SIZE'] = 10
 app.config['COMPRESS_MIMETYPES'] = ['application/json']
+Compress(app)
 
 def login():
     data = request.json
