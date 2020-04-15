871ae7e4c58048c750ecc428491e6b714666b876
joeyqy wang <hypercycle@litatom.com>
2020-04-15 12:37:29 +0800

    test

--- litatom/service/anti_spam_rate_service.py
+++ litatom/service/anti_spam_rate_service.py
@@ -101,6 +101,7 @@ class AntiSpamRateService(object):
     def out_of_times(cls, key, num):
         stop_num = redis_client.get(key)
         stop_num = 0 if not stop_num else int(stop_num)
+        print stop_num, num
         ''' 先判断 再往上加的  所以第二次 stop_num 为 1 第三次 为 2'''
         return stop_num >= num
 
