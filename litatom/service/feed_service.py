5741ac8ef448a3f0432dea624d0e914127cdfe99
joeyqy wang <hypercycle@litatom.com>
2020-04-15 21:05:32 +0800

    test

--- litatom/service/feed_service.py
+++ litatom/service/feed_service.py
@@ -62,6 +62,7 @@ class FeedService(object):
             return False
         status = Feed.objects(user_id=user_id, create_time__gte=judge_time).count() <= 3
         if not status and setting.IS_DEV:
+            AntiSpamRateService.inform_spam(user_id)
             return True
         return status
 
