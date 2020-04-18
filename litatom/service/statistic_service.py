24f81730f2edd408cdbe6b2dc9d71269bb5ec9d8
joeyqy wang <hypercycle@litatom.com>
2020-04-18 11:55:58 +0800

    test

--- litatom/service/statistic_service.py
+++ litatom/service/statistic_service.py
@@ -471,6 +471,7 @@ class DiamStatService(object):
         for item in list:
             resp = AliLogService.get_log_atom(from_time=from_time, to_time=to_time, query=list[item],
                                               project=project, logstore=logstore)
+            print 'hhhhhh'
             try:
                 for log in resp.logs:
                     contents = log.get_contents()
