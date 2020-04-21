7ead7aec7cfe50c239c250fe75a692bf89870dba
joeyqy wang <hypercycle@litatom.com>
2020-04-21 18:14:23 +0800

    test

---
+++ back_procs/down_files.py
@@ -0,0 +1,16 @@
+import  os
+
+def down():
+    f = open('tt').read().split('\n')
+    urls = [el.split('data\\":\\"')[1].split('\\"}"')[0] for el in f]
+    cnt = 0
+    save_dir = 'data/musics/'
+    for _ in urls:
+        cnt += 1
+        cmd = "wget \'%s\' -O %s" % (_, save_dir + str(cnt) + '.mp4')
+        print cmd
+        os.system(cmd)
+
+
+if __name__ == '__main__':
+    down()
