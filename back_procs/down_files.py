df6302ca7b280398e824760cecea9b11e62ae2b4
joeyqy wang <hypercycle@litatom.com>
2020-04-21 19:39:33 +0800

    test

--- back_procs/down_files.py
+++ back_procs/down_files.py
@@ -2,7 +2,11 @@ import  os
 
 def down():
     f = open('tt').read().split('\n')
-    urls = [el.split('data\\":\\"')[1].split('\\"}"')[0] for el in f if len(el) > 10]
+    urls = []
+    for el in f:
+        print el
+        url = el.split('data\\":\\"')[1].split('\\"}"')[0]
+        print url
     cnt = 0
     save_dir = 'data/musics/'
     for _ in urls:
