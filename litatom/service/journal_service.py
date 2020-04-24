5f4f19d30271c598874245f4f60c4b6d7f9d544a
joeyqy wang <hypercycle@litatom.com>
2020-04-24 15:16:48 +0800

    test

--- litatom/service/journal_service.py
+++ litatom/service/journal_service.py
@@ -47,6 +47,7 @@ class JournalService(object):
             objs = UserSetting.objects().limit(1000)
         cnt = 1
         for obj in objs:
+            obj.lang
             # cls.USER_LOC[obj.user_id] = obj.lang
             cnt += 1
             if cnt % 10000 == 0:
