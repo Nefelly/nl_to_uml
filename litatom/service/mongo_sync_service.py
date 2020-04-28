375b586c7b06ed95ce43a209e7f44141544b55b3
joeyqy wang <hypercycle@litatom.com>
2020-04-28 21:11:14 +0800

    test

--- litatom/service/mongo_sync_service.py
+++ litatom/service/mongo_sync_service.py
@@ -24,7 +24,7 @@ class MongoSyncService(object):
         save_add = os.path.join(dir_name, '%s_%d.csv' % (table_name, int(time.time())))
         host, port, user, pwd, db, auth_db = get_args_from_db(db)
         fields = 'user_id'
-        sql = '''mongoexport -h {host} --port {port} -u {user} -p {pwd} --authenticationDatabase {auth_db} -d {db_name} -c {collection} -f {fileds} --type=csv --out {save_add}'''.format(
+        sql = '''mongoexport -h {host} --port {port} -u {user} -p {pwd} --authenticationDatabase {auth_db} -d {db_name} -c {collection} -f {fields} --type=csv --out {save_add}'''.format(
             host=host, port=port, user=user, pwd=pwd, auth_db=auth_db, db_name=db_name, collection=table_name, fields=fields, save_add=save_add)
         print sql
         os.system(sql)
