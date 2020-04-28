0c76486183a8094509550906953fe2d9a152ed95
joeyqy wang <hypercycle@litatom.com>
2020-04-28 20:35:58 +0800

    test

--- litatom/service/experiment_service.py
+++ litatom/service/experiment_service.py
@@ -144,7 +144,7 @@ class ExperimentService(object):
         old_noset_num = len(old_noset)
 
         def add_new(value, num, old_noset):
-            buckets  = sys_rnd.sample(old_noset, num)
+            buckets = sys_rnd.sample(old_noset, num)
             for b in buckets:
                 print 'creating', exp_name, b, value
                 ExpBucket.create(exp_name, b, value)
