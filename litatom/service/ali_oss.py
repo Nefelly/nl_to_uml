57caa7e588d1251d118cd291a99ae643859b54f2
joeyqy wang <hypercycle@litatom.com>
2020-04-30 02:21:48 +0800

    test

--- litatom/service/ali_oss.py
+++ litatom/service/ali_oss.py
@@ -135,7 +135,7 @@ class AliOssService(object):
         try:
             blob = Blob(obj)
             im = Image(blob)
-            im.quality(80)
+            im.quality(50)
             im.filterType(FilterTypes.SincFilter)
             x = im.size().width()
             y = im.size().height()
