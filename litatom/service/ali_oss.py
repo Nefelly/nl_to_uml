2bc4afdf1f33c69934a1fc18d8c196523cbd50a8
joeyqy wang <hypercycle@litatom.com>
2020-04-30 00:30:03 +0800

    test

--- litatom/service/ali_oss.py
+++ litatom/service/ali_oss.py
@@ -128,7 +128,7 @@ class AliOssService(object):
         f.close()
         print src_add
         im = Image(src_add)
-        im.quality(100)
+        im.quality(35)
         im.filterType(FilterTypes.SincFilter)
         x = im.size().width()
         y = im.size().height()
