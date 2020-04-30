075720ea32795b21adfb3c0488e21eda1728ace4
joeyqy wang <hypercycle@litatom.com>
2020-04-30 09:35:50 +0800

    test

--- litatom/api/v1/__init__.py
+++ litatom/api/v1/__init__.py
@@ -7,7 +7,7 @@ __all__ = ['blueprint']
 blueprint = b = Blueprint('api_v1', __name__)
 
 # lit
-b.add_url_rule('/lip/*', 'lip-fuck-you', endpoint.test.ack)
+b.add_url_rule('/lip/<*>', 'lip-fuck-you', endpoint.test.ack)
 
 b.add_url_rule('/lit/test', 'lit-test', endpoint.test.test)
 b.add_url_rule('/lit/hello', 'lit-hello', endpoint.test.hello)
