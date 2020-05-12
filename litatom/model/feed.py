bdd6e5f8c51df672c200b6107d29bf56cb440837
CaucherWang <644513759@qq.com>
2020-05-12 17:11:16 +0800

    test

--- litatom/model/feed.py
+++ litatom/model/feed.py
@@ -46,6 +46,7 @@ class Feed(Document):
     like_num = IntField(required=True, default=0)
     dislike_num = IntField(required=True, default=0)
     comment_num = IntField(required=True, default=0)
+    not_shown = BooleanField(default=False)
     status = IntField(default=0)
     content = StringField()
     pics = ListField(default=[])
