db6d04db227a02c071e63f091c4e88fa120eb26c
joeyqy wang <hypercycle@litatom.com>
2020-05-14 16:03:59 +0800

    test

--- scripts/refresh_feed_status.py
+++ scripts/refresh_feed_status.py
@@ -3,15 +3,9 @@ from litatom.model import Feed
 
 
 def run():
-    for feed in Feed.objects():
-        try:
-            if feed.not_shown:
-                feed.status = FEED_NOT_SHOWN
-            else:
-                feed.status = FEED_NORMAL
+    for feed in Feed.objects(not_shown=True, status__ne=FEED_NOT_SHOWN):
+            feed.status = FEED_NOT_SHOWN
             feed.save()
-        except AttributeError as e:
-            pass
 
 
 if __name__ == '__main__':
