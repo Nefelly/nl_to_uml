3a34fae180172c5f23faeb06306e7af37649efcb
joeyqy wang <hypercycle@litatom.com>
2020-05-22 17:31:23 +0800

    test

--- litatom/api/v1/__init__.py
+++ litatom/api/v1/__init__.py
@@ -27,6 +27,7 @@ b.add_url_rule('/lit/user/avatars', 'user-get-avatars', endpoint.user.get_avatar
 b.add_url_rule('/lit/user/info_by_huanxin', 'user-info-by-huanxin', endpoint.user.user_info_by_huanxinids, methods=['POST'])
 b.add_url_rule('/lit/user/messages', 'user-messages', endpoint.user.user_messages)
 b.add_url_rule('/lit/user/read_message/<message_id>', 'user-message-read', endpoint.user.read_message)
+b.add_url_rule('/lit/user/read_all_messages', 'user-read_all_messages', endpoint.user.read_all_messages)
 b.add_url_rule('/lit/user/firebase_token', 'user-firebase-token', endpoint.user.register_firebase, methods=['POST'])
 b.add_url_rule('/lit/user/firebase_push', 'user-firebase-push', endpoint.user.firebase_push, methods=['POST'])
 b.add_url_rule('/lit/user/query_online', 'user-query-online', endpoint.user.query_online, methods=['POST'])
