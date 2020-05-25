931c6f75d56823dd6cea322e40b03a5273270e0f
joeyqy wang <hypercycle@litatom.com>
2020-05-25 12:14:01 +0800

    test

--- litatom/api/v1/endpoint/home.py
+++ litatom/api/v1/endpoint/home.py
@@ -194,7 +194,7 @@ def settings():
 
 
 def check_version():
-    version_now = '3.2.4'
+    version_now = '3.4.5'
     version = request.args.get('version', None)
     if 0 and GlobalizationService.get_region() == GlobalizationService.REGION_TH:
         message = u'กรุณาอัพเดทเวอร์ชั่น เราได้ทำการแก้ไขปัญหาส่งข้อความเรียบร้อยแล้ว ขอบคุณค่ะ'
