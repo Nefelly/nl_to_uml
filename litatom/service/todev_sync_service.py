020afc5f450d98ab5d16515a90970246f47bb64e
joeyqy wang <hypercycle@litatom.com>
2020-05-28 16:04:14 +0800

    test

--- litatom/service/todev_sync_service.py
+++ litatom/service/todev_sync_service.py
@@ -81,10 +81,10 @@ class ToDevSyncService(object):
                 query_str[f] = tmp
             if not query_field:
                 query_field = fields
-            print query_str, query_field
             real_query = '%s.objects(%s).first()' % (model.__name__, ','.join([query_str[el] for el in query_field]))
             print real_query
             res = eval(real_query)
+            print res, '!!!!'
             if res:
                 res.delete()
             to_save = eval('%s(%s)' % (model.__name__, ','.join (query_str.values())))
