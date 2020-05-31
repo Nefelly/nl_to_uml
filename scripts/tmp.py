c6d5db8cb3b83fb98b892029ffdfd0ab60d80c62
CaucherWang <644513759@qq.com>
2020-05-31 21:11:40 +0800

    .

--- scripts/tmp.py
+++ scripts/tmp.py
@@ -1,5 +1,5 @@
 from litatom.model import UserSetting, UserRecord
-from litatom.service import ForbidRecordService
+from litatom.service import MongoSyncService
 
 
 def get_device_forbidden_num_by_uid(uuid):
@@ -14,15 +14,16 @@ def get_device_forbidden_num_by_uid(uuid):
 num = 0
 res = 0
 x = list()
-for user in UserSetting.objects():
-    uuid = user.uuid
+table = MongoSyncService.load_table_map(UserSetting, 'user_id', 'uuid')
+for user in table:
+    uuid = table[user]
     if not uuid or uuid in x:
         continue
     x.append(uuid)
     num += 1
     if get_device_forbidden_num_by_uid(uuid) >= 3:
         res += 1
-        if res % 10000 ==0:
-            print(num,res)
+        if res % 10000 == 0:
+            print(num, res)
 
 print(num, res)
