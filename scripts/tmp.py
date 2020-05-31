ef3646353f7d8519c7089f990e1d5be94b4f395d
CaucherWang <644513759@qq.com>
2020-05-31 21:04:58 +0800

    .

--- scripts/tmp.py
+++ scripts/tmp.py
@@ -1,6 +1,7 @@
 from litatom.model import UserSetting, UserRecord
 from litatom.service import ForbidRecordService
 
+
 def get_device_forbidden_num_by_uid(uuid):
     uids = UserSetting.get_uids_by_uuid(uuid)
 
@@ -10,11 +11,16 @@ def get_device_forbidden_num_by_uid(uuid):
     return reduce(add, map(UserRecord.get_forbidden_num_by_uid, uids))
 
 
-num=0
-res=0
-for uuid in UserSetting.objects().distinct('uuid'):
-    num+=1
-    if get_device_forbidden_num_by_uid(uuid) >=3:
-        res+=1
+num = 0
+res = 0
+x = list()
+for user in UserSetting.objects():
+    uuid = user.uuid
+    if not uuid or uuid in x:
+        continue
+    x.append(uuid)
+    num += 1
+    if get_device_forbidden_num_by_uid(uuid) >= 3:
+        res += 1
 
-print(num,res)
+print(num, res)
