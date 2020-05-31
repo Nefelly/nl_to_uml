ae359204209304c1912014e2ddab8c422ef51d36
CaucherWang <644513759@qq.com>
2020-05-31 20:21:59 +0800

    .

--- scripts/refresh_report.py
+++ scripts/refresh_report.py
@@ -2,12 +2,16 @@ from litatom.model import Report
 
 def run():
     reports = Report.objects(create_ts__gte=1589904000,dealed=False)
+    print(len(reports))
+    deal_num = 0
     for report in reports:
         if report.reason == 'match':
             report.forbid_weight = 2
         else:
             report.forbid_weight = 4
         report.save()
+        deal_num+=1
+    print(deal_num)
 
 if __name__ == '__main__':
     run()
