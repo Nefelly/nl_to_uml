82765495a76be759fbb8baa44482c82d20012175
CaucherWang <644513759@qq.com>
2020-06-02 20:23:23 +0800

    test

--- scripts/exp_ana.py
+++ scripts/exp_ana.py
@@ -41,10 +41,10 @@ def get_active_uids(from_date, to_date):
     return uids
 
 
-def load_uid_payment(default_uids, exp_uids):
+def load_uid_payment(default_uids, exp_uids,from_time,to_time):
     resp = AliLogService.get_log_atom(project='litatom-account', logstore='account_flow',
-                                      from_time='2020-04-28 00:00:00+8:00',
-                                      to_time='2020-05-07 00:00:00+8:00',
+                                      from_time=AliLogService.datetime_to_alitime(from_time),
+                                      to_time=AliLogService.datetime_to_alitime(to_time),
                                       query="name:deposit | SELECT user_id,sum(diamonds) as res GROUP by user_id limit 500000")
     default_pay_sum = 0
     exp_pay_sum = 0
@@ -63,6 +63,86 @@ def load_uid_payment(default_uids, exp_uids):
 
     return default_pay_sum, exp_pay_sum, len(default_pay_num), len(exp_pay_num)
 
+
+def get_exp_res(default_uids, exp_uids, from_date, to_date):
+    len_default_uids = len(default_uids)
+    len_exp_uids = len(exp_uids)
+
+    default_pay_sum, exp_pay_sum, default_pay_num, exp_pay_num = load_uid_payment(default_uids, exp_uids, from_date, to_date)
+
+    print('payment average of default group:', default_pay_sum / len_default_uids,
+          'payment user rate average of default group:', default_pay_num / len_default_uids)
+    print('payment average of experiment group:', exp_pay_sum / len_exp_uids,
+          'payment user rate average of experiment group:', exp_pay_num / len_exp_uids)
+
+
+    i = 0
+
+    sum_default_rate = 0.0
+    sum_exp_rate = 0.0
+
+    origin_len_default_uids = len_default_uids
+    origin_len_exp_uids = len_exp_uids
+    default_retain_res = [(origin_len_default_uids,1)]
+    exp_retain_res = [(origin_len_exp_uids,1)]
+    while from_date <= next_date(to_date, -1):
+        i += 1
+        nextdate = next_date(from_date)
+        active_uids = get_active_uids(from_date, nextdate)
+        default_uids = active_uids & set(default_uids)
+        new_len_default_uids = len(default_uids)
+        default_retain_rate = new_len_default_uids / len_default_uids
+        print(from_date, nextdate, 'retain rate of default group: ', new_len_default_uids, '/', len_default_uids, '=',
+              default_retain_rate,)
+        default_retain_res.append((new_len_default_uids,default_retain_rate))
+        len_default_uids = float(new_len_default_uids)
+
+        exp_uids = active_uids & set(exp_uids)
+        new_len_exp_uids = len(exp_uids)
+        exp_retain_rate = new_len_exp_uids / len_exp_uids
+        print(from_date, nextdate, 'retain rate of experiment group: ', new_len_exp_uids, '/', len_exp_uids, '=',
+              exp_retain_rate,)
+        exp_retain_res.append((new_len_exp_uids,exp_retain_rate))
+        len_exp_uids = float(new_len_exp_uids)
+
+        sum_default_rate += default_retain_rate
+        sum_exp_rate += exp_retain_rate
+
+        from_date = nextdate
+
+    print('total ', i, 'days')
+    print('average retain rate of default group:', sum_default_rate / i)
+    print('average retain rate of experiment group:', sum_exp_rate / i)
+
+    write_data('')
+
+
+def write_data(name, sheet_names, tb_heads, data):
+    """
+
+    :param name:
+    :param sheet_names:
+    :param tb_heads:
+    :param data:一个列表，列表中的每个元素也是一个列表，每个元素都是1个sheet中的数据
+    :return:
+    """
+    import xlwt
+    f = xlwt.Workbook(encoding='utf-8')
+    # 建表
+    sheets = [f.add_sheet(s) for s in sheet_names]
+    # 写表头
+    for sheet in sheets:
+        for i in range(len(tb_heads)):
+            sheet.write(i, 0, tb_heads[i])
+
+    for col_num in range(len(data)):
+        col_data = data[col_num]
+        for sheet_num in range(len(col_data)):
+            sheet_col_data = col_data[sheet_num]
+            sheet = sheets[sheet_num]
+            for row_num in range(len(sheet_col_data)):
+                sheet.write(row_num, col_num + 1, sheet_col_data[row_num])
+    f.save(name)
 
 def run():
     exp_uids = read_uids_from_file('/data/exp/match-428/exp_ids')
@@ -85,47 +165,6 @@ def run():
     len_default_uids = float(len(default_uids))
     print(len_default_uids)
 
-    default_pay_sum, exp_pay_sum, default_pay_num, exp_pay_num = load_uid_payment(default_uids, exp_uids)
-
-    print('payment average of default group:', default_pay_sum / len_default_uids,
-          'payment user rate average of default group:', default_pay_num / len_default_uids)
-    print('payment average of experiment group:', exp_pay_sum / len_exp_uids,
-          'payment user rate average of experiment group:', exp_pay_num / len_exp_uids)
-
-    from_date = parse_standard_time('2020-04-28 00:00:00')
-    to_date = parse_standard_time('2020-05-04 00:00:00')
-
-    i = 0
-
-    sum_default_rate = 0.0
-    sum_exp_rate = 0.0
-
-    while from_date <= to_date:
-        i += 1
-        nextdate = next_date(from_date)
-        active_uids = get_active_uids(from_date, nextdate)
-        default_uids = active_uids & set(default_uids)
-        new_len_default_uids = len(default_uids)
-        default_retain_rate = new_len_default_uids / len_default_uids
-        print(from_date, nextdate, 'retain rate of default group: ', new_len_default_uids, '/', len_default_uids, '=',
-              default_retain_rate,)
-        len_default_uids = float(new_len_default_uids)
-
-        exp_uids = active_uids & set(exp_uids)
-        new_len_exp_uids = len(exp_uids)
-        exp_retain_rate = new_len_exp_uids / len_exp_uids
-        print(from_date, nextdate, 'retain rate of experiment group: ', new_len_exp_uids, '/', len_exp_uids, '=',
-              exp_retain_rate,)
-        len_exp_uids = float(new_len_exp_uids)
-
-        sum_default_rate += default_retain_rate
-        sum_exp_rate += exp_retain_rate
-
-        from_date = nextdate
-
-    print('total ', i, 'days')
-    print('average retain rate of default group:', sum_default_rate / i)
-    print('average retain rate of experiment group:', sum_exp_rate / i)
 
 
 if __name__ == '__main__':
