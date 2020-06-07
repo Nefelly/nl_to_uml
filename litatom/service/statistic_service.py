9b19153d5c28117d042c84d74a5388de887e4dd2
CaucherWang <644513759@qq.com>
2020-06-07 14:52:24 +0800

    ,

--- litatom/service/statistic_service.py
+++ litatom/service/statistic_service.py
@@ -555,9 +555,12 @@ class DiamStatService(object):
 
         for loc in data:
             if loc == 'ALL':
-                data[loc].append(User.objects(vip_time__gte=date_to_int_time(date)).count())
+                data[loc].append(User.objects(vip_time__gte=date_to_int_time(next_date(date,30*ONE_DAY)),
+                                              vip_time__lte=date_to_int_time(next_date(date,31*ONE_DAY))).count())
             else:
-                data[loc].append(User.objects(vip_time__gte=date_to_int_time(date),country=loc).count())
+                data[loc].append(User.objects(vip_time__gte=date_to_int_time(next_date(date,30*ONE_DAY)),
+                                              vip_time__lte=date_to_int_time(next_date(date,31*ONE_DAY)),
+                                              country=loc).count())
 
         for loc in data:
             excel_dic = cls.cal_stats_from_list(cls.STAT_QUERY_LIST, from_time, to_time, loc=loc)
