5105c72394385d2f94411732bbeed1011802201e
joeyqy wang <hypercycle@litatom.com>
2020-06-08 18:58:43 +0800

    test

--- scripts/material_upload.py
+++ scripts/material_upload.py
@@ -29,6 +29,7 @@ def up_paied_avatar():
         tmp = os.path.join(dirName, g)
         for f in os.listdir(tmp):
             fileName = os.path.join(tmp, f)
+            print fileName
             fileid = AliOssService.upload_from_binary(open(fileName).read())
             Avatar.create(fileid, g, True, 5)
 
