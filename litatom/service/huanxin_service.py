7ba492c86a1bf6593c99fd200aec9e0a614c0f8e
joeyqy wang <hypercycle@litatom.com>
2020-06-28 12:20:31 +0800

    test

--- litatom/service/huanxin_service.py
+++ litatom/service/huanxin_service.py
@@ -410,6 +410,7 @@ class HuanxinService(object):
         }
         try:
             response = requests.delete(url, verify=False, headers=headers).json()
+            print response
             assert response.get('entities')[0]['username']
             return True
         except Exception, e:
