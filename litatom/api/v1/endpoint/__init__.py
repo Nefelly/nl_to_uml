3f02ba12733b4b1f55022afcb2b34ef98a71e702
CaucherWang <644513759@qq.com>
2020-06-29 19:27:16 +0800

    ,

--- litatom/api/v1/endpoint/__init__.py
+++ litatom/api/v1/endpoint/__init__.py
@@ -1,23 +1,3 @@
 # flake8: noqa
-from . import test
-from . import sms_code
-from . import user
-from . import oss
-from . import huanxin
-from . import home
-from . import debug
-from . import anoy_match
-from . import feed
-from . import user_relations
-from . import admin
-from . import activity
-from . import voice_chat
-from . import ad
-from . import account
-from . import experiment
-from . import user_visit
-from . import match_history
 from . import nl
-from . import gift
-from . import user_tag
-from . import outer_hook
+
